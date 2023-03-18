cffcd480e0134033f82ad0c870fcf4d000e68b4a
dunkin <dk6781.lee@gmail.com>
2023-03-18 11:48:03 +0900

    add Links

--- 0308/Solution.java
+++ 0308/Solution.java
@@ -1,53 +1,55 @@
-    public class Solution {
-        int[] curMax = {};
-        public int solution(int[][] triangle) {
-            int answer = 0;
+//문제  https://school.programmers.co.kr/learn/courses/30/lessons/43105
+public class Solution {
+    int[] curMax = {};
 
-            // sample
-            // [[7],
-            //[3, 8],
-            //[8, 1, 0],
-            //[2, 7, 4, 4],
-            //[4, 5, 2, 6, 5]]
+    public int solution(int[][] triangle) {
+        int answer = 0;
 
-            for (int[] is2 : triangle) { //triangle[]
+        // sample
+        // [[7],
+        // [3, 8],
+        // [8, 1, 0],
+        // [2, 7, 4, 4],
+        // [4, 5, 2, 6, 5]]
 
-                calcMax(is2);
+        for (int[] is2 : triangle) { // triangle[]
 
-            }
+            calcMax(is2);
 
-            for (int val : curMax) {
-                if(answer <= val) {
-                    answer = val;
-                }
-            }
-
-            return answer;
         }
 
-        public void calcMax(int[] curArray) {
-            int[] nextMax = new int[curMax.length + 1];
-            if (curMax.length == 0) {
-                nextMax[0] = curArray[0];
-            } else {
-                int index = 0;
-                for (int currValue : curArray) {
-                    if (index == 0) {
-                        nextMax[index] = currValue + curMax[index];
-                    } else if (index == curArray.length - 1) {
+        for (int val : curMax) {
+            if (answer <= val) {
+                answer = val;
+            }
+        }
+
+        return answer;
+    }
+
+    public void calcMax(int[] curArray) {
+        int[] nextMax = new int[curMax.length + 1];
+        if (curMax.length == 0) {
+            nextMax[0] = curArray[0];
+        } else {
+            int index = 0;
+            for (int currValue : curArray) {
+                if (index == 0) {
+                    nextMax[index] = currValue + curMax[index];
+                } else if (index == curArray.length - 1) {
+                    nextMax[index] = currValue + curMax[index - 1];
+                } else {
+                    if (curMax[index - 1] > curMax[index]) {
                         nextMax[index] = currValue + curMax[index - 1];
                     } else {
-                        if (curMax[index-1] > curMax[index]) {
-                            nextMax[index] = currValue + curMax[index-1];
-                        } else {
-                            nextMax[index] = currValue + curMax[index];
-                        }
+                        nextMax[index] = currValue + curMax[index];
                     }
-                    index++;
                 }
+                index++;
             }
-
-            //max값 업데이트
-            curMax = nextMax;
         }
+
+        // max값 업데이트
+        curMax = nextMax;
     }
+}
