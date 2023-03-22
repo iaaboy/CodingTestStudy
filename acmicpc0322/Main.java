c33233007466bb7ed22b27cda75187601076dbd2
dunkin <dk6781.lee@gmail.com>
2023-03-22 22:14:51 +0900

    서식 맞춤.

--- acmicpc0322/Main.java
+++ acmicpc0322/Main.java
@@ -10,45 +10,46 @@ import java.util.HashSet;
 import java.util.Vector;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int inN = Integer.parseInt(br.readLine());
 
         new Main().solution(inN);
     }
+
     public void solution(int n) { // N 출력
-        //System.out.println("n: " + n);
-        Set <Integer> num = new HashSet<>();
-        for(int i = 0; i< n ; i++) {
-            num.add(i+1);
+        // System.out.println("n: " + n);
+        Set<Integer> num = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            num.add(i + 1);
         }
-        //System.out.println(num);
+        // System.out.println(num);
         perm(new Vector<>(), num);
     }
 
-    private void perm(Vector <Integer> curArray, Set <Integer> LeftNum){ //LeftNum 는 정렬되어있음.
-        if(LeftNum.size() == 1) {
+    private void perm(Vector<Integer> curArray, Set<Integer> LeftNum) { // LeftNum 는 정렬되어있음.
+        if (LeftNum.size() == 1) {
 
-            Vector <Integer> newArrPrint = new Vector<>(curArray);
+            Vector<Integer> newArrPrint = new Vector<>(curArray);
             newArrPrint.addAll(LeftNum);
-            //System.out.println(newArrPrint);
+            // System.out.println(newArrPrint);
 
-            for(int i : newArrPrint) {
+            for (int i : newArrPrint) {
                 System.out.print(i + " ");
             }
             System.out.println();
             return;
-            //마지막
+            // 마지막
         }
 
-        for(int n : LeftNum) {
-            Vector <Integer> newArr = new Vector<>(curArray);
+        for (int n : LeftNum) {
+            Vector<Integer> newArr = new Vector<>(curArray);
             Set<Integer> newLeftNum = new HashSet<>(LeftNum);
             newArr.add(n);
             newLeftNum.remove(n);
 
-            //System.out.println("new" + newArr);
-            //System.out.println("left" + newLeftNum);
+            // System.out.println("new" + newArr);
+            // System.out.println("left" + newLeftNum);
 
             perm(newArr, newLeftNum);
             newArr.removeElement(n);
