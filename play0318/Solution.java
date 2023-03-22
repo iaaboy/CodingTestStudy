442d4235a8aa08ad4b827346e076e79c2897d3f1
dunkin <dk6781.lee@gmail.com>
2023-03-23 02:17:58 +0900

    최종 수정 fail

--- play0318/Solution.java
+++ play0318/Solution.java
@@ -1,22 +1,16 @@
 package play0318;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Iterator;
 import java.util.Vector;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
-public class Solution {
-
+class Solution {
     class Node {
-        Node Node(int y, int x) {
+        Node(int y, int x) {
             this.y = y;
             this.x = x;
-            return this;
         }
 
         int y;
@@ -24,10 +18,10 @@ public class Solution {
     }
 
     class Group {
-        Group Group(int gId) {
+        Group(int gId) {
             this.gId = gId;
+            groupSum = 0;
             nodes = new Vector<>();
-            return this;
         }
 
         int gId;
@@ -44,13 +38,12 @@ public class Solution {
     int myArr[][];
     int groupIDs[][];
     Integer Idx;
-    HashMap<Integer, Group> groups;
+    HashMap<Integer, Group> groups = new HashMap<>();
 
     public int[] solution(String[] maps) {
 
         myArr = new int[maps.length][maps[0].length()];
         groupIDs = new int[maps.length][maps[0].length()];
-        groups = new HashMap<>();
         Idx = 0;
 
         for (int y = 0; y < maps.length; y++) {
@@ -64,19 +57,17 @@ public class Solution {
             }
         }
 
-        for (int[] line : myArr) {
-            System.out.println("mid result: " + Arrays.toString(line));
-        }
+        // for (int[] line : myArr) {
+        //     // System.out.println("mid result: " + Arrays.toString(line));
+        // }
 
         for (int y = 0; y < myArr.length; y++) {
             for (int x = 0; x < myArr[y].length; x++) {
                 boolean checkResult = false;
-
                 if (myArr[y][x] == -1) {
                     groupIDs[y][x] = -1;
                     continue;
                 }
-
                 if (x != 0) {
                     checkResult = checkLeft(y, x);
                 }
@@ -84,8 +75,8 @@ public class Solution {
                     checkResult = checkTop(y, x) || checkResult;
                 }
                 if (!checkResult) {
-                    Group g = new Group().Group(++Idx);
-                    Node nd = new Node().Node(y, x);
+                    Group g = new Group(++Idx);
+                    Node nd = new Node(y, x);
                     g.nodes.add(nd);
                     g.groupSum = myArr[y][x];
                     groups.put(Idx, g);
@@ -94,17 +85,29 @@ public class Solution {
             }
         }
 
-        Set<Integer> answerSet = new HashSet<>();
-        int i=0;
+        ArrayList answerSet = new ArrayList<Integer>();
+        int i = 0;
+        int[] answer;
+
         groups.forEach((key, value) -> {
-            System.out.println("value.groupSum : " + value.groupSum + i);
             answerSet.add(value.groupSum);
+            value.nodes.clear();
         });
+        groups.clear();
+        Collections.sort(answerSet);
 
-        int[] answer = new int[0];
+        // System.out.println("value.groupSum : " + answerSet);
 
-        if(answerSet.size() > 0) {
-            answerSet.toArray();
+        if (answerSet.size() > 0) {
+            answer = new int[answerSet.size()];
+            Iterator<Integer> iter = answerSet.iterator();
+            // Returns an iterator over the elements
+            while (iter.hasNext()) {
+                answer[i] = iter.next();
+                // System.out.println("value : " + answer[i] );
+                i++;
+            }
+
         } else {
             answer = new int[1];
             answer[0] = -1;
@@ -112,6 +115,7 @@ public class Solution {
 
 
         return answer;
+
     }
 
     boolean checkTop(int y, int x) {
@@ -120,9 +124,9 @@ public class Solution {
         if (myArr[y - 1][x] != -1) {
             if (groupIDs[y][x] == 0) {
                 groupIDs[y][x] = groupIDs[y - 1][x];
-                addToGroup(groupIDs[y - 1][x], new Node().Node(y, x));
+                addToGroup(groupIDs[y - 1][x], new Node(y, x));
             } else {
-                if(groupIDs[y][x] != groupIDs[y - 1][x])
+                if (groupIDs[y][x] != groupIDs[y - 1][x])
                     combineToGroup(groupIDs[y][x], groupIDs[y - 1][x]);
             }
             result = true;
@@ -138,13 +142,14 @@ public class Solution {
             groupIDs[n.y][n.x] = gTo;
         }
         groups.get(gTo).groupSum += groups.get(gFrom).groupSum;
+        groups.get(gFrom).nodes.clear();
         groups.remove(gFrom);
     }
 
     boolean checkLeft(int y, int x) {
         if (myArr[y][x - 1] != -1) {
             groupIDs[y][x] = groupIDs[y][x - 1];
-            addToGroup(groupIDs[y][x - 1], new Node().Node(y, x));
+            addToGroup(groupIDs[y][x - 1], new Node(y, x));
             return true;
         }
 
@@ -153,7 +158,7 @@ public class Solution {
 
     void addToGroup(int gId, Node nd) {
         if (groups.get(gId) == null) {
-            System.out.println("Somthing wrong");
+            // System.out.println("Somthing wrong");
         }
         groups.get(gId).groupSum += myArr[nd.y][nd.x];
         groups.get(gId).nodes.add(nd);
--- play0318/TestMain_0318.java
+++ play0318/TestMain_0318.java
@@ -1,5 +1,7 @@
 package play0318;
 
+import java.util.Arrays;
+
 public class TestMain_0318 {
 
     /*
@@ -10,14 +12,14 @@ public class TestMain_0318 {
     
         public static void main(String[] args) {
     
-            String maps[] = {"X591X","XXX5X","X231X", "1XXX1"};
+            String maps[] = {"15X1X","11X5X","1XXXX", "1XXX1"};
             String maps2[] = {"XXX","XXX","XXX"};
             String maps3[] = {"X591X","X1X5X","X231X", "1XXX1"};
             
             Solution mSolution = new Solution();
-            System.out.println("maps result" + mSolution.solution(maps));
-            System.out.println("maps2 result" + mSolution.solution(maps2));
-            System.out.println("maps3 result" + mSolution.solution(maps3));
+            System.out.println("maps result" + Arrays.toString(mSolution.solution(maps)));
+            System.out.println("maps2 result" + Arrays.toString(mSolution.solution(maps2)));
+            System.out.println("maps3 result" + Arrays.toString(mSolution.solution(maps3)));
         }
 }
     
