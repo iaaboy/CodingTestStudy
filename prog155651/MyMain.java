10657c7b1c5f39c8c8ac616220854e65910649a3
dunkin <dk6781.lee@gmail.com>
2023-04-22 12:41:17 +0900

    Programmers 호텔 대실 문제 풀이

--- prog155651/MyMain.java
+++ prog155651/MyMain.java
@@ -1,9 +1,12 @@
 package prog155651;
 
+import java.util.*;
+
 public class MyMain {
     public static void main(String[] args) {
-        //["15:00", "17:00"], ["16:40", "18:20"], ["14:20", "15:20"], ["14:10", "19:20"], ["18:20", "21:20"]
-        String[][] bTime = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
+        String[][] bTime = { { "09:10", "10:10" }, { "10:10", "12:20" } };
+        // String[][] bTime = {{"15:00", "17:00"}, {"16:40", "18:20"}, {"14:20",
+        // "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
 
         Solution mSol = new Solution();
         mSol.solution(bTime);
@@ -12,34 +15,70 @@ public class MyMain {
 
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 0;
+        ArrayList<TimeLog> timeLog = new ArrayList<TimeLog>();
 
-        for(String[] bt : book_time){
-            convertTime(bt[0]);
-            convertTime(bt[1]);
+        for (String[] bt : book_time) {
+            timeLog.add(convertTime(bt[0], 1));
+            timeLog.add(convertTime(bt[1], -1));
         }
 
-        return answer;
+        timeLog.sort((it, that) -> {
+            if (it.time > that.time) {
+                return 1;
+            } else if (it.time == that.time) {
+                if (it.sig < that.sig) {
+                    return -1;
+                } else
+                    return 1;
+            } else {
+                return -1;
+            }
+        });
+
+        int curMax = 0;
+        int curDepth = 0;
+        for (TimeLog tl : timeLog) {
+            if (tl.sig > 0) {
+                curDepth++;
+                if (curDepth > curMax) {
+                    curMax = curDepth;
+                }
+            } else {
+                curDepth--;
+            }
+        }
+
+        System.out.println(timeLog);
+        System.out.println(curMax);
+
+        return curMax;
     }
-    private int convertTime(String time){
+
+    private TimeLog convertTime(String time, int sig) {
         String[] numString = time.split(":");
-        
 
         int hour = Integer.parseInt(numString[0]);
-        System.out.println(hour);
         int min = Integer.parseInt(numString[1]);
-        System.out.println(min);
-        int result = hour*60+min;
-        System.out.println(result);
-        return result;
+        int result = hour * 60 + min;
+        if (sig == -1) {
+            result += 10;
+        }
+        // System.out.println(result);
+        return new TimeLog(result, sig);
     }
 }
 
-class TimeLog{
-    int time; //시작 시간
-    int startEnd; //start인지 end인지. 1 / -1
-    public TimeLog(int time, int startEnd) {
+class TimeLog {
+    int time; // 시작 시간
+    int sig; // 시작(1)인지, 종료(-1)인지.
+
+    public TimeLog(int time, int sig) {
         this.time = time;
-        this.startEnd = startEnd;
+        this.sig = sig;
+    }
+
+    @Override
+    public String toString() {
+        return time + "," + sig;
     }
 }
