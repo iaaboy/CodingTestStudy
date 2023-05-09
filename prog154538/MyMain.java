3892a1d8b00161a1c60237e3ca2ee4f9ccacf44e
dunkin <dk6781.lee@gmail.com>
2023-05-10 00:52:20 +0900

    recursive 시도(실패)

--- prog154538/MyMain.java
+++ prog154538/MyMain.java
@@ -2,7 +2,7 @@ package prog154538;
 
 public class MyMain {
     public static void main(String[] args) {
-        int[][] numbers = { { 3,3, 1} };
+        int[][] numbers = { { 2, 100, 4} };
 
         Solution mSol = new Solution();
 
@@ -14,58 +14,59 @@ public class MyMain {
 }
 
 class Solution {
+    int Y, X, N;
+    int curN ;
+    int cur2 ;
+    int cur3 ;
     public int solution(int x, int y, int n) {
-        if (x == y) {
+        this.Y = y;
+        this.X = x;
+        this.N = n;
+        curN = cur2 = cur3 = X;
+        if(x==y)
             return 0;
-        }
-        int rN = checkAdd(x, y, n);
-        int r2 = (int) log2(y, x);
-        int r3 = (int) log3(y, x);
 
-         System.out.println("addN:" + rN);
-         System.out.println("log2: " + r2);
-         System.out.println("log3:" + r3);
+        int rN = checkN(x, y, n);
+        int result = Math.min(rN, checkNext(1));
 
-        return getMin(rN, r2, r3);
-    }
-
-    private int getMin(int n1, int n2, int n3) {
-        int result = Math.min(Math.min(n1, n2), n3);
-        if (result == Integer.MAX_VALUE) {
+        if(result == Integer.MAX_VALUE) {
             return -1;
-        } else
-            return result;
+        } else 
+        return result;
     }
 
-    private int checkAdd(int x, int y, int n) {
+    private int checkN(int x, int y, int n) {
         if (((y - x) % n != 0)) {
             return Integer.MAX_VALUE;
         } else {
             return (y - x) / n;
         }
     }
-
-    private double log2(int y, int x) {
-        if (y % x != 0) {
+    
+    private int checkNext(int n){
+        if((cur2 == Integer.MAX_VALUE) && (cur3 == Integer.MAX_VALUE)) {
             return Integer.MAX_VALUE;
         } else {
-            double res = Math.log(y / x) / Math.log(2);
-            if (res % 1 == 0) {
-                return res;
-            } else
-                return Integer.MAX_VALUE;
-        }
-    }
+            if(cur2 != Integer.MAX_VALUE && cur2 < Y) {
+                cur2 = cur2*2;
+            } else if(cur2 != Integer.MAX_VALUE && cur2 > Y) {
+                cur2 = Integer.MAX_VALUE;
+            }
 
-    private double log3(int y, int x) {
-        if (y % x != 0) {
-            return Integer.MAX_VALUE;
-        } else {
-            double res = Math.log(y / x) / Math.log(3);
-            if (res % 1 == 0) {
-                return res;
-            } else
-                return Integer.MAX_VALUE;
+            if(cur3 != Integer.MAX_VALUE && cur3 < Y) {
+                cur3 = cur3*3;
+            } else if(cur2 != Integer.MAX_VALUE && cur3 > Y) {
+                cur3 = Integer.MAX_VALUE;
+            }
+
+            System.out.println("nums:" + curN + "," + cur2 + "," + cur3);
+            if((cur2 == Y) || (cur3 == Y)) {
+                return n;
+            }
+
+            return checkNext(n + 1);
         }
     }
 }
+
+
