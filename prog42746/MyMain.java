efdac31d0b1c90acd75823948bc6cd16a7c28691
dunkin <dk6781.lee@gmail.com>
2023-05-21 16:56:47 +0900

    12

--- prog42746/MyMain.java
+++ prog42746/MyMain.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MyMain {
     public static void main(String[] args) {
         int[][] inArr = {
-                { 104, 1 }
+                { 0,1,2,3,5}
         };
 
         Solution mSol = new Solution();
@@ -18,29 +18,34 @@ public class MyMain {
 class Solution {
     public String solution(int[] num) {
         String answer = "";
-        String[] nString = new String[num.length];
+        String [] nString = new String[num.length];
         int idx = 0;
         int count = 0;
 
-        for (int n : num) {
+        for(int n : num){
             nString[idx++] = Integer.toString(n);
-            if (n == 0) {
+            if(n==0) {
                 count++;
             }
         }
 
-        if (count == num.length) {
+        if(count == num.length) {
             return "0";
         }
 
-        Arrays.sort(nString, (aStr, bStr) -> {
-            return Integer.parseInt(aStr + bStr) < Integer.parseInt(bStr + aStr) ? 1 : -1;
-        });
+        if(nString.length >1 )
+            Arrays.sort(nString, (aStr,bStr) -> {
+                if(Integer.parseInt(aStr + bStr) < Integer.parseInt(bStr + aStr)) {
+                    return 1;
+                } else
+                return -1;
+            });
 
-        for (String aa : nString) {
-            System.out.println(aa);
-            answer += aa;
+        for(String aa : nString) {
+            //System.out.println(aa);
+            answer+=aa;
         }
+        System.out.println(nString);
 
         return answer;
     }
