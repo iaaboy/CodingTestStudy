8bda4e60cc6dfa90f7bc564464e7067854a458c6
dunkin <dk6781.lee@gmail.com>
2023-06-03 12:54:06 +0900

    사칙연산 풀이. ... ㅠ.ㅠ

--- prog1843/MyMain.java
+++ prog1843/MyMain.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class MyMain {
     public static void main(String[] args) {
         String[] nums = { "5", "-", "3", "+", "1", "+", "2", "-", "4" };
+        // String[] nums = { "1", "-", "3", "+", "5", "-", "8" };
 
         Solution mSol = new Solution();
         System.out.println(mSol.solution(nums));
@@ -12,87 +13,60 @@ public class MyMain {
 }
 
 class Solution {
+    int operandsCount;
+    int[][] max_dp;
+    int[][] min_dp;
+
     public int solution(String arr[]) {
-        int answer = -1;
-        ArrayList<Integer> num = new ArrayList<Integer>();
-        ArrayList<Character> op = new ArrayList<Character>();
+        // 연산자 수
+        operandsCount = arr.length / 2 + arr.length % 2;
 
-        // 인풋을 두개 에레이로 분리
-        boolean isOdd = true;
-        for (String inS : arr) {
-            if (isOdd) {
-                num.add(Integer.parseInt(inS));
-            } else {
-                op.add(inS.charAt(0));
+        // max / min dp 할당
+        max_dp = new int[operandsCount][operandsCount];
+        min_dp = new int[operandsCount][operandsCount];
+
+        // 최대, 최소값으로 초기화
+        for (int i = 0; i < operandsCount; i++) {
+            for (int k = 0; k < operandsCount; k++) {
+                max_dp[i][k] = Integer.MIN_VALUE;
+                min_dp[i][k] = Integer.MAX_VALUE;
             }
-            isOdd = !isOdd;
         }
-        // System.out.println(num);
-        // System.out.println(op);
 
-        // 계산(함수)
-        answer = calc(num, op);
+        // 초기값을 string에서 parse
+        for (int i = 0; i < operandsCount; i++) {
+            max_dp[i][i] = Integer.parseInt(arr[i * 2]);
+            min_dp[i][i] = Integer.parseInt(arr[i * 2]);
+        }
 
-        return answer;
+        // printdp();
+
+        // 원소 1번부터 다음 원소 계산
+        for (int cnt = 1; cnt < operandsCount; cnt++) {
+            printdp();
+            for (int i = 0; i < operandsCount - cnt; i++) {
+                int j = i + cnt;
+                for (int k = i; k < j; k++) {
+                    if (arr[k * 2 + 1].charAt(0) == '+') {
+                        max_dp[i][j] = Math.max(max_dp[i][j], max_dp[i][k] + max_dp[k + 1][j]);
+                        min_dp[i][j] = Math.min(min_dp[i][j], min_dp[i][k] + min_dp[k + 1][j]);
+                    } else {
+                        max_dp[i][j] = Math.max(max_dp[i][j], max_dp[i][k] - min_dp[k + 1][j]);
+                        min_dp[i][j] = Math.min(min_dp[i][j], min_dp[i][k] - max_dp[k + 1][j]);
+                    }
+                }
+            }
+        }
+
+        return max_dp[0][operandsCount - 1];
     }
 
-    int calc(ArrayList<Integer> num, ArrayList<Character> op) {
-
-        int index = 0;
-        int max = Integer.MIN_VALUE;
-        int calcResult = 0;
-
-        if (op.size() == 1) {
-            if (op.get(0) == '+') { // +
-                calcResult = num.get(0) + num.get(1);
-            } else { // -
-                calcResult = num.get(0) - num.get(1);
-            }
-            if (calcResult > max) {
-                max = calcResult;
-            }
-
-            // System.out.println("max :" + max);
-            return max;
+    void printdp() {
+        for (int[] dpArr : max_dp) {
+            System.out.println(Arrays.toString(dpArr));
         }
-
-        for (char operator : op) {
-            int tempResult = 0;
-            if (operator == '+') { // +
-                tempResult = num.get(index) + num.get(index + 1);
-            } else { // -
-                tempResult = num.get(index) - num.get(index + 1);
-            }
-            index++;
-
-            // recursively calc호출
-            // num 생성
-            ArrayList<Integer> newNum = new ArrayList<>();
-            for (Integer n : num) {
-                newNum.add(n);
-            }
-            newNum.remove(index - 1);
-            newNum.remove(index - 1);
-            newNum.add(index - 1, tempResult);
-            // 값 넣어주기
-
-            // op 생성
-            ArrayList<Character> newOp = new ArrayList<>();
-            for (char b : op) {
-                newOp.add(b);
-            }
-            newOp.remove(index - 1);
-            // 값 넣어주기
-
-            // System.out.println(newNum);
-            // System.out.println(newOp);
-            calcResult = calc(newNum, newOp);
-            if (calcResult > max) {
-                max = calcResult;
-            }
+        for (int[] dpArr : min_dp) {
+            System.out.println(Arrays.toString(dpArr));
         }
-
-        // System.out.println("max :" + max);
-        return max;
     }
 }
