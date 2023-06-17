83f3517ebfedbd1b1c8c6e3a22afe12ae422a8e6
dunkin <dk6781.lee@gmail.com>
2023-06-17 12:50:27 +0900

    마법의 엘리베이터 풀이

---
+++ prog87694/MyMain.java
@@ -0,0 +1,14 @@
+package prog87694;
+
+public class MyMain {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        int answer = 0;
+        return answer;
+    }
+}
