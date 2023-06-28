66d08d7479e392a391085f4d70c50383b80f3398
dunkin <dk6781.lee@gmail.com>
2023-06-28 22:53:48 +0900

    아이템 줍기 풀이

--- prog87694/MyMain.java
+++ prog87694/MyMain.java
@@ -6,20 +6,25 @@ public class MyMain {
     public static void main(String[] args) {
         int[][][] rectangle = {
                 { { 1, 1, 7, 4 }, { 3, 2, 5, 5 }, { 4, 3, 6, 9 }, { 2, 6, 8, 8 } },
-                { { 1, 1, 8, 4 }, { 2, 2, 4, 9 }, { 3, 6, 9, 8 }, { 6, 3, 7, 7 } }, { { 1, 1, 5, 7 } },
-                { { 2, 1, 7, 5 }, { 6, 4, 10, 10 } }, { { 2, 2, 5, 5 }, { 1, 3, 6, 4 }, { 3, 1, 4, 6 } },
+                { { 1, 1, 8, 4 }, { 2, 2, 4, 9 }, { 3, 6, 9, 8 }, { 6, 3, 7, 7 } },
+                { { 1, 1, 5, 7 } },
+                { { 2, 1, 7, 5 }, { 6, 4, 10, 10 } },
+                { { 2, 2, 5, 5 }, { 1, 3, 6, 4 }, { 3, 1, 4, 6 } }
         };
         int ip[][] = {
-                { 1, 3, 7, 8 }, { 9, 7, 6, 1 }, { 1, 1, 4, 7 }, { 3, 1, 7, 10 }, { 1, 4, 6, 3 },
+                { 1, 3, 7, 8 },
+                { 9, 7, 6, 1 },
+                { 1, 1, 4, 7 },
+                { 3, 1, 7, 10 },
+                { 1, 4, 6, 3 }
         };
-        Solution mSol = new Solution();
-        // for (int i = 0; i < rectangle.length; i++) {
-        // System.out.println(mSol.solution(rectangle[i], ip[i][0], ip[i][1], ip[i][2],
-        // ip[i][3]));
-        // }
 
-        int i = 1;
-        System.out.println(mSol.solution(rectangle[i], ip[i][0], ip[i][1], ip[i][2], ip[i][3]));
+        // int i = 2;
+
+        for (int i = 0; i < 5; i++) {
+            Solution mSol = new Solution();
+            System.out.println(mSol.solution(rectangle[i], ip[i][0], ip[i][1], ip[i][2], ip[i][3]));
+        }
     }
 }
 
@@ -30,21 +35,21 @@ class Solution {
         int answer = 0;
         rects = new Rect[rectangle.length];
         for (int i = 0; i < rectangle.length; i++) {
-            rects[i] = new Rect(rectangle[i][0], rectangle[i][1], rectangle[i][2], rectangle[i][3], i);
+            rects[i] = new Rect(rectangle[i][0] * 2, rectangle[i][1] * 2, rectangle[i][2] * 2, rectangle[i][3] * 2, i);
         }
         // set contact
         setNeighbor();
 
         // System.out.println(allPtr);
-        System.out.println(rects[0] + "\n");
+        // System.out.println(rects[0] + "\n");
         // System.out.println(rects[1] + "\n");
         // System.out.println(rects[2] + "\n");
         // System.out.println(rects[3] + "\n");
         // System.out.println(Arrays.toString(rects));
 
-        answer = trip(characterX, characterY, itemX, itemY);
+        answer = trip(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
 
-        System.out.println("Result: " + answer);
+        // System.out.println("Result: " + answer);
         return answer;
     }
 
@@ -66,8 +71,15 @@ class Solution {
         }
     }
 
+    int midResult = -1;
+    int fromX;
+    int fromY;
+
     private int trip(int fromX, int fromY, int toX, int toY) {
         int result = 0;
+
+        this.fromX = fromX;
+        this.fromY = fromY;
 
         // 도형을 찾는다.
         Point curP = null;
@@ -82,42 +94,62 @@ class Solution {
                 break;
         }
 
-        System.out.println("start Y,X: " + curP);
-        while (!(fromX == toX && fromY == toY)) {
-            if(curP.contact != -1) {
+        // System.out.println("start Y,X: " + curP);
+        while (!(curP.isVisited && curP.x == fromX && curP.y == fromY)) {
+            if (curP.contact != -1) {
                 curP = nextPoint(curP, true);
             } else {
                 curP = nextPoint(curP, false);
             }
-            System.out.println("goNext: " + curP);
             result++;
+            // System.out.println("goNext: " + curP + ",r: " + result);
+
+            if (curP.x == toX && curP.y == toY) {
+                midResult = result;
+                // System.out.println("result: " + result);
+            }
         }
+        // System.out.println("final result: " + result);
+
+        result = Math.min(midResult / 2, (result - midResult) / 2);
 
         return result;
     }
 
     Point nextPoint(Point curP, boolean isCross) {
         int rectId = curP.rectId;
-        if(isCross) {
+        if (isCross) {
             rectId = curP.contact;
+            for (Point p : rects[rectId].outline) {
+                if (curP.x == p.x && curP.y == p.y) {
+                    p.isVisited = true;
+                }
+            }
         }
         for (Point p : rects[rectId].outline) {
-            if (p.rectId != -1 && ((curP.x == p.x && Math.abs(curP.y - p.y) == 1) || curP.y == p.y && Math.abs(curP.x - p.x) == 1)) {
-                if(isCross) {
-                    //상대 도형 내부인지 확인
-                    System.out.println(curP);
-                    System.out.println(p);
+            if (!p.isVisited && p.rectId != -1 && ((curP.x == p.x && Math.abs(curP.y - p.y) == 1)
+                    || curP.y == p.y && Math.abs(curP.x - p.x) == 1)) {
+                if (isCross) {
+                    // 상대 도형 내부인지 확인
+                    // System.out.println(curP);
+                    // System.out.println(p);
 
-                    System.out.println(rects[curP.rectId].p1);
-                    System.out.println(rects[curP.rectId].p2);
+                    // System.out.println(rects[curP.rectId].p1);
+                    // System.out.println(rects[curP.rectId].p2);
 
-                    if((rects[curP.rectId].p1.x <= p.x &&  rects[curP.rectId].p2.x >= p.x
-                        && rects[curP.rectId].p1.y <= p.y &&  rects[curP.rectId].p2.y >= p.y)) {
-                        System.out.println("pass: " + p);
+                    if ((rects[curP.rectId].p1.x <= p.x && rects[curP.rectId].p2.x >= p.x
+                            && rects[curP.rectId].p1.y <= p.y && rects[curP.rectId].p2.y >= p.y)) {
+                        // System.out.println("pass: " + p);p.isVisited = true;
                         continue;
                     }
 
                 }
+
+                if (midResult < 0 && (p.x == fromX) && (p.y == fromY)) {
+                    continue;
+                }
+
+                p.isVisited = true;
                 return p;
             }
         }
@@ -155,6 +187,7 @@ class Point implements Comparable<Point> {
     int y;
     int rectId;
     int contact;
+    boolean isVisited = false;
 
     public Point(int y, int x, int rectId) {
         this.x = x;
@@ -174,6 +207,6 @@ class Point implements Comparable<Point> {
 
     @Override
     public String toString() {
-        return "(" + y + "," + x + ":" + rectId + "," + contact + ")";
+        return "(" + x + "," + y + ":" + rectId + "," + contact + ")";
     }
 }
