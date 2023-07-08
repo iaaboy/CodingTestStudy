4a9b27229e929d1f1d5d43aafb561b9a8ce38b1d
dunkin <dk6781.lee@gmail.com>
2023-07-08 12:49:52 +0900

    억억단을 외우자

--- prog138475/MyMain.java
+++ prog138475/MyMain.java
@@ -4,46 +4,71 @@ import java.util.*;
 
 public class MyMain {
     public static void main(String[] args) {
-        int e = 20;
+        int e = 10;
         int[] starts = { 1, 3, 7 };
-
         Solution mSol = new Solution();
-
         System.out.println(Arrays.toString(mSol.solution(e, starts)));
     }
 }
 
 class Solution {
     public int[] solution(int e, int[] starts) {
+        int[] submultiCounts = new int[e + 1];
+        int[] maxIndex = new int[e + 1];
         int[] answer = new int[starts.length];
-        HashMap<Integer, Integer> numCount = new HashMap<>();
 
-        for (int x = 1; x <= e; x++) {
-            for (int y = 1; y <= e; y++) {
-                int multy = x * y;
-                if (multy > e) {
-                    break;
-                }
-                System.out.print(x + "," + y + "->" + multy + "   ");
-                if (numCount.containsKey(multy)) {
-                    numCount.put(multy, numCount.get(multy) + 1);
-                } else {
-                    numCount.put(multy, 1);
-                }
+        int minST = Integer.MAX_VALUE;
+        for (int st : starts) {
+            if (minST > st) {
+                minST = st;
             }
-            System.out.println();
         }
 
-        System.out.println(numCount);
+        // 조금 빠른 약수 counting
+        // for (int i = minST; i <= e; i++) {
+        // submultiCounts[i] = submulti(i);
+        // }
+
+        // 가장 빠른 약수 counting
+        for (int i = 1; i <= e; i++) {
+            for (int j = 1; j <= e / i; j++) {
+                submultiCounts[i * j]++;
+            }
+        }
+
+        // System.out.println(Arrays.toString(submultiCounts));
+
+        int curMax = Integer.MIN_VALUE;
+        int curMaxIndex = submultiCounts.length - 1;
+        for (int m = submultiCounts.length - 1; m >= minST; m--) {
+            if (submultiCounts[m] >= curMax) {
+                curMax = submultiCounts[m];
+                curMaxIndex = m;
+            }
+            maxIndex[m] = curMaxIndex;
+        }
+        // System.out.println(Arrays.toString(maxIndex));
+
         for (int i = 0; i < starts.length; i++) {
-            int curMax = Integer.MIN_VALUE;
-            for (int m = starts[i]; m <= e; m++) {
-                if (numCount.get(m) != null && numCount.get(m) > curMax) {
-                    curMax = numCount.get(m);
-                    answer[i] = m;
-                }
+            answer[i] = maxIndex[starts[i]];
+        }
+
+        return answer;
+    }
+
+    int submulti(int num) {
+
+        int count = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (i * i == num) {
+                count++;
+            } else if (num % i == 0) {
+                count += 2;
             }
         }
-        return answer;
+
+        // System.out.println("num:" + num + ", count" + count);
+
+        return count;
     }
 }
