889e7432b81795434a449694f71ffaea15de6ce6
dunkin <dk6781.lee@gmail.com>
2023-08-10 22:21:49 +0900

    등굣길 2

--- prog214288/MyMain.java
+++ prog214288/MyMain.java
@@ -4,15 +4,21 @@ import java.util.*;
 
 public class MyMain {
     public static void main(String[] args) {
-        int k = 3;
-        int n = 5;
-        int[][] reqs = { { 10, 60, 1 }, { 15, 100, 3 }, { 20, 30, 1 }, { 30, 50, 3 },
-                { 50, 40, 1 }, { 60, 30, 2 },
-                { 65, 30, 1 }, { 70, 100, 2 } }; // 25
+        // int k = 3;
+        // int n = 5;
+        // int[][] reqs = { { 10, 60, 1 }, { 15, 100, 3 }, { 20, 30, 1 }, { 30, 50, 3 },
+        //         { 50, 40, 1 }, { 60, 30, 2 },
+        //         { 65, 30, 1 }, { 70, 100, 2 } }; // 25
         // int k = 2;
         // int n = 3;
         // int[][] reqs = { { 5, 55, 2 }, { 10, 90, 2 }, { 20, 40, 2 }, { 50, 45, 2 }, {
         // 100, 50, 2 } }; // 90
+
+        int k = 1;
+        int n = 1;
+        int[][] reqs = { { 10, 60, 1 }, { 15, 100, 1 }, { 20, 30, 1 }, { 30, 50, 1 },
+                { 50, 40, 1 }, { 60, 30, 1 },
+                { 65, 30, 1 }, { 70, 100, 1 } }; // 25
 
         Solution mSol = new Solution();
         System.out.println("answer: " + mSol.solution(k, n, reqs));
@@ -22,12 +28,13 @@ public class MyMain {
 class Solution {
     int[][] delayList;
     ArrayList<ArrayList<Consult>> cList;
+
     public int solution(int k, int n, int[][] reqs) {
         int[] assigned = new int[k];
         delayList = new int[k][n - k + 1];
 
-        for(int i = 0; i < k;i++)
-            for(int j = 0; j < n-k + 1; j++)
+        for (int i = 0; i < k; i++)
+            for (int j = 0; j < n - k + 1; j++)
                 delayList[i][j] = -1;
 
         cList = new ArrayList<>();
@@ -48,8 +55,8 @@ class Solution {
 
     int minAssigned = Integer.MAX_VALUE;
 
-    int getDelay(int y, int x){
-        if(delayList[y][x] == -1) {
+    int getDelay(int y, int x) {
+        if (delayList[y][x] == -1) {
             if (cList.get(y).size() == 0) {
                 delayList[y][x] = 0;
             } else {
@@ -69,7 +76,6 @@ class Solution {
             int sum = 0;
             for (int i = 0; i < assigned.length; i++) {
                 sum += getDelay(i, assigned[i] - 1);
-                //sum += delayList[i][assigned[i] - 1];
             }
 
             // System.out.println("sum: " + sum + ", " + Arrays.toString(assigned));
@@ -77,7 +83,7 @@ class Solution {
 
             return;
         }
-        for (int i = 0; i < assigned.length; i++) {
+        for (int i = assigned.length - 1; i >= 0 ; i--) {
             assigned[i]++;
             makeSelections(countRemained - 1, assigned);
             assigned[i]--;
@@ -87,18 +93,21 @@ class Solution {
     private int getDelayedTime(ArrayList<Consult> arrayList, int max) {
         int delay = 0;
         int index = 0;
-        int now = 0;
+        int now = arrayList.get(0).st;
         PriorityQueue<Consult> mQ = new PriorityQueue<>((a, b) -> {
             return a.ed - b.ed;
         });
 
+        // System.out.println("max: " + max + ": " + arrayList);
+
+
         while (index <= arrayList.size() || !mQ.isEmpty()) {
 
-            // System.out.println(now + ": " + arrayList);
+            // System.out.println("now: " + now + " , arrayList: " + arrayList);
 
             // 1 queue check
-            if (!mQ.isEmpty() && now >= mQ.peek().ed) {
-                System.out.println(now + ": done" + mQ);
+            while (!mQ.isEmpty() && now >= mQ.peek().ed) {
+                // System.out.println(now + ": done" + mQ);
                 mQ.poll();
 
                 if (index == arrayList.size() && mQ.isEmpty()) {
@@ -107,16 +116,22 @@ class Solution {
             }
 
             // 2 list check
-            if (index < arrayList.size()) {
+            while (index < arrayList.size()) {
                 if (mQ.size() < max) {
                     arrayList.get(index).ed = arrayList.get(index).st + arrayList.get(index).delay
                             + arrayList.get(index).dur;
+                    // System.out.println ("startWork : " + arrayList.get(index));
                     mQ.add(arrayList.get(index));
                     index++;
-                } else {
-                    arrayList.get(index).delay += (mQ.peek().ed - arrayList.get(index).st);
-                    delay += arrayList.get(index).delay;
-                    // System.out.println("addDelay: " + arrayList.get(index).delay);
+                } else { //대기를 늘려야함.
+                    int start = arrayList.get(index).st + arrayList.get(index).delay;
+                    if(start <= now) {
+                        arrayList.get(index).delay += (mQ.peek().ed - arrayList.get(index).st);
+                        delay += arrayList.get(index).delay;
+                        // System.out.println("addDelay: " + arrayList.get(index).delay);
+                    } else {
+                        break;
+                    }
                     // delayed
                 }
             }
@@ -128,10 +143,16 @@ class Solution {
                 } else
                     now = Math.min(mQ.peek().ed, arrayList.get(index).st + arrayList.get(index).delay);
             } else {
-                now = arrayList.get(index).st + arrayList.get(index).delay;
+                if (index >= arrayList.size()) {
+                    break;
+                } else {
+                    now = arrayList.get(index).st + arrayList.get(index).delay;
+                }
+                
             }
         }
 
+        // System.out.println("delay: " + delay);
         return delay;
     }
 }
--- prog42898_2/MyMain.java
+++ prog42898_2/MyMain.java
@@ -13,6 +13,8 @@ package prog42898_2;
  * 잘못된 입력은 없음
  */
 
+import java.util.*;
+
 public class MyMain {
     public static void main(String[] args) {
         int m = 4;
@@ -24,29 +26,78 @@ public class MyMain {
     }
 }
 
+// class Solution {
+//     public int solution(int m, int n, int[][] puddles) {
+//         int answer = 0;
+//         int[][] map = new int[n + 1][m + 1];
+//         int devider = 1000000007;
+
+//         for (int[] p : puddles)
+//             map[p[1]][p[0]] = -1; // 웅덩이
+
+//         map[1][1] = 1;
+
+//         for (int y = 1; y <= n; y++)
+//             for (int x = 1; x <= m; x++) {
+//                 if ((x == 1 && y == 1) || (map[y][x] == -1))
+//                     continue;
+//                 if (map[y][x - 1] != -1)
+//                     map[y][x] += map[y][x - 1];
+//                 if (map[y - 1][x] != -1)
+//                     map[y][x] += map[y - 1][x];
+//                 map[y][x] = map[y][x] % devider;
+//             }
+
+//         answer = map[n][m];
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int[][] map = new int[n + 1][m + 1];
-        int devider = 1000000007;
+        
+        int[][] dp = new int[n+1][m+1];
+        boolean[][] visited = new boolean[n+1][m+1];
+        
+        // 방향순서 : 상 -> 하 -> 좌 -> 우
+        int[] dy = new int[]{ +1, 0};
+        int[] dx = new int[]{ 0, +1};
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{1, 1});
+        
+        for(int[] puddle : puddles) {
+            dp[puddle[1]][puddle[0]] = -1;    // dp배열에 물웅덩이 표시
+        }
 
-        for (int[] p : puddles)
-            map[p[1]][p[0]] = -1; // 웅덩이
+        // bfs
+        while(!q.isEmpty()) {
+            int[] poll = q.poll();
+            int y = poll[0];
+            int x = poll[1];
+            
+            for(int i = 0; i < dy.length; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
 
-        map[1][1] = 1;
-
-        for (int y = 1; y <= n; y++)
-            for (int x = 1; x <= m; x++) {
-                if ((x == 1 && y == 1) || (map[y][x] == -1))
-                    continue;
-                if (map[y][x - 1] != -1)
-                    map[y][x] += map[y][x - 1];
-                if (map[y - 1][x] != -1)
-                    map[y][x] += map[y - 1][x];
-                map[y][x] = map[y][x] % devider;
+                if(ny >= 1 && ny <= n && nx >= 1 && nx <= m) {
+                    if(dp[ny][nx] == -1) continue;  // 물웅덩이인 경우 continue
+                   
+                    if(!visited[ny][nx]) {
+                        dp[ny][nx] = (dp[y][x] + 1) % 1000000007;
+                        visited[ny][nx] = true;
+                        q.offer(new int[]{ny, nx});   
+                        System.out.println(nx + ", " + ny);
+                    }
+                    
+                }
             }
+            
+        }
 
-        answer = map[n][m];
-        return answer;
+        for(int i = 0; i< dp.length ; i++)
+            System.out.println(Arrays.toString(dp[i]));
+        
+        return dp[n][m] - 1;
     }
 }
