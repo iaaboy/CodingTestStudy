1b506d55a3997303d5c9fa48bd0b8b6b0ab2505d
dunkin <dk6781.lee@gmail.com>
2023-08-24 00:04:48 +0900

    모음사전 성능향상

--- prog84512_2/MyMain.java
+++ prog84512_2/MyMain.java
@@ -1,13 +1,15 @@
 package prog84512_2;
 
+import java.util.ArrayList;
+
 /* 모음사전
  * https://school.programmers.co.kr/learn/courses/30/lessons/84512
  */
 
 public class MyMain {
     public static void main(String[] args) {
-        String word = "AAAAE";
-        // String word = "EIO";
+        // String word = "AAAE";
+        String word = "EIO";
 
         Solution mSol = new Solution();
         System.out.println(mSol.solution(word));
@@ -16,43 +18,40 @@ public class MyMain {
 
 class Solution {
     String[] wordTable = { "A", "E", "I", "O", "U" };
-    String word;
+    ArrayList<String> dict = makeMine(new ArrayList<>());
 
     public int solution(String word) {
+        return dict.indexOf(word) + 1;
+    }
+
+    ArrayList<String> makeMine (ArrayList<String> mDict) {
         String curWord = "";
-        this.word = word;
-        int result = 0;
+        for(int i = 0; i< 10000 ; i++) {
+            System.out.println(i);
+        }
 
         for (int i = 0; i < wordTable.length; i++) {
             curWord += wordTable[i];
-            result = searchDict(curWord, 0);
-            if (result > 0)
-                return result;
+            makeDict(mDict, curWord, 0);
             curWord = curWord.substring(0, curWord.length() - 1);
         }
 
-        return result;
+        mDict.sort(null);
+        return mDict;
     }
 
-    int count = 0;
-
-    int searchDict(String curWd, int depth) {
-        int result = -1;
-        if (word.equals(curWd)) {
-            return count + 1;
-        }
-        count++;
-        if (depth >= 4) {
-            return result;
+    // int count = 0;
+    void makeDict(ArrayList<String> mDict, String curWd, int depth) {
+        mDict.add(curWd);
+        // if (count++ < 100)
+        //     System.out.println(curWd);
+        if (depth == 4) {
+            return;
         }
         for (int i = 0; i < wordTable.length; i++) {
             curWd += wordTable[i];
-            result = searchDict(curWd, depth + 1);
+            makeDict(mDict, curWd, depth + 1);
             curWd = curWd.substring(0, curWd.length() - 1);
-
-            if (result > 0)
-                break;
         }
-        return result;
     }
 }
