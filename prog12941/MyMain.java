e2ad1ea168a04772183f7f129a140402e38f2b74
dunkin <dk6781.lee@gmail.com>
2024-01-19 14:03:28 +0900

    최솟값 만들기 풀이

---
+++ prog12941/MyMain.java
@@ -0,0 +1,35 @@
+package prog12941;
+
+import java.util.*;
+
+/* 최솟값 만들기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12941
+ */
+
+public class MyMain {
+    public static void main(String[] args) {
+        int [][] sample = {
+            {1,4,2}, {5,4,4}
+        };
+        Solution mSol = new Solution();
+        
+        System.out.println(mSol.solution(sample[0], sample[1]));
+    }
+}
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i = 0; i < A.length ; i++) {
+            answer += A[i] * B[B.length - i - 1];
+        }
+
+        return answer;
+    }
+}
