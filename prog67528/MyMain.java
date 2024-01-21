6e7eef3405249c3c7fd372279c747d27103a9f93
iaaboy <dk6781.lee@gmail.com>
2024-01-21 16:05:11 +0900

    Test commit

---
+++ prog67528/MyMain.java
@@ -0,0 +1,17 @@
+package prog67528;
+
+public class MyMain {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public int[] solution(String[] gems) {
+        int[] answer = {};
+
+        
+
+        return answer;
+    }
+}
