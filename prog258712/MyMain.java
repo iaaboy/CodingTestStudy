2b3efb329c26862d77ea0b89b0a4b2e07e5a5af8
dunkin <dk6781.lee@gmail.com>
2024-01-24 21:52:42 +0900

    가장 많이 받은 선물 풀이

---
+++ prog258712/MyMain.java
@@ -0,0 +1,89 @@
+package prog258712;
+
+import java.util.*;
+
+/* 가장 많이 받은 선물
+ * https://school.programmers.co.kr/learn/courses/30/lessons/258712
+ */
+
+public class MyMain {
+    public static void main(String[] args) {
+        String[][] freiendsList = {
+                { "muzi", "ryan", "frodo", "neo" },
+                { "joy", "brad", "alessandro", "conan", "david" },
+                { "a", "b", "c" }
+        };
+        String[][] giftsList = {
+                { "muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan",
+                        "neo muzi" },
+                { "alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david" },
+                { "a b", "b a", "c a", "a c", "a c", "c a" }
+        };
+
+        Solution mSol = new Solution();
+        ;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("answer:" + mSol.solution(freiendsList[i], giftsList[i]));
+        }
+    }
+}
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int fCount = friends.length;
+        HashMap<String, Integer> nameMap = new HashMap<>();
+        int[][] giftMap = new int[fCount][fCount];
+        int[] presentIndex = new int[fCount];
+        int[] presentNextMonth = new int[fCount];
+
+        //이름 -> index로 변환
+        for (int i = 0; i < fCount; i++) {
+            nameMap.put(friends[i], i);
+        }
+
+        //주고 받은 선물표
+        for (String g : gifts) {
+            String[] names = g.split(" ");
+            giftMap[nameMap.get(names[0])][nameMap.get(names[1])]++;
+        }
+
+        //선물 지수 계산
+        for (int i = 0; i < fCount; i++) {
+            for (int j = 0; j < fCount; j++) {
+                presentIndex[i] += giftMap[i][j]; //i가 j에게 줌
+                presentIndex[i] -= giftMap[j][i]; //i가 j로부터 받음
+            }
+        }
+
+        for (int i = 0; i < fCount; i++) {
+            for (int j = 0; j < fCount; j++) {
+                if (giftMap[i][j] > giftMap[j][i]) {  //서로 주고받은 것 계산
+                    presentNextMonth[i]++;
+                } else if (i > j /* 두번 호출 방지 */ && giftMap[i][j] == giftMap[j][i]) { // 같을 때에는 선물 지수 비교
+                    if (presentIndex[i] > presentIndex[j]) {
+                        presentNextMonth[i]++;
+                    } else if (presentIndex[i] < presentIndex[j]) {
+                        presentNextMonth[j]++;
+                    }
+                }
+            }
+        }
+
+        //최대값을 answer로
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < fCount; i++) {
+            if (presentNextMonth[i] > answer) {
+                answer = presentNextMonth[i];
+            }
+        }
+
+        // for (int i = 0; i < fCount; i++) {
+        // System.out.println(Arrays.toString(giftMap[i]));
+        // }
+        // System.out.println();
+        // System.out.println(Arrays.toString(presentIndex));
+        // System.out.println(Arrays.toString(presentNextMonth));
+
+        return answer;
+    }
+}
---
+++ prog42893/MyMain.java
@@ -0,0 +1,23 @@
+package prog42893;
+
+/* 매칭 점수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42893
+ */
+
+public class MyMain {
+    public static void main(String[] args) {
+        
+    }
+}
+
+//기본점수는 해당 웹페이지의 텍스트 중, 검색어가 등장하는 횟수이다. (대소문자 무시)
+//외부 링크 수는 해당 웹페이지에서 다른 외부 페이지로 연결된 링크의 개수이다.
+//한 웹페이지의 링크점수는 해당 웹페이지로 링크가 걸린 다른 웹페이지의 기본점수 ÷ 외부 링크 수의 총합이다.
+//한 웹페이지의 매칭점수는 기본점수와 링크점수의 합으로 계산한다.
+
+class Solution {
+    public int solution(String word, String[] pages) {
+        int answer = 0;
+        return answer;
+    }
+}
