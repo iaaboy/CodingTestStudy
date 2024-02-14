ed47c7633fcd90f9bdbb8a5107cd7a0c90e58fa0
iaaboy <dk6781.lee@gmail.com>
2024-02-15 01:02:40 +0900

    중간최선.

--- prog72413/MyMain.java
+++ prog72413/MyMain.java
@@ -24,20 +24,15 @@ public class MyMain {
 }
 
 class Solution {
-    int a, b;
-    int[][] minCost;
-
     public int solution(int n, int s, int a, int b, int[][] fares) {
         Vertex[] ver = new Vertex[n + 1];
         boolean[] visited = new boolean[n + 1]; // n은 1부터..
-        minCost = new int[n + 1][n + 1];
+        int[][] minCost = new int[n + 1][n + 1];
         for (int i = 0; i < n + 1; i++) {
-            Arrays.fill(minCost[i], Integer.MAX_VALUE);
+            Arrays.fill(minCost[i], Integer.MAX_VALUE - 1);
         }
-        this.a = a;
-        this.b = b;
         for (int i = 0; i < n + 1; i++) {
-            ver[i] = new Vertex(i);
+            ver[i] = new Vertex();
         }
 
         for (int[] fare : fares) {
@@ -47,75 +42,49 @@ class Solution {
 
         visited[s] = true;
         PriorityQueue<Next> pQ = new PriorityQueue<>((me, you) -> me.cost - you.cost);
-        pQ.add(new Next(s, s, 0, visited));
+        pQ.add(new Next(s, s, 0));
 
         while (!pQ.isEmpty()) {
             Next current = pQ.poll();
-            printNext("handle", current);
+            // printNext("handle", current);
             if (current.nowA == a && current.nowB == b) {
                 return current.cost;
             }
 
             if (current.nowA == current.nowB) { // 다음도 합승
                 for (Node nd : ver[current.nowA].nodes) {
-                    if (!current.visited[nd.to] && (minCost[nd.to][nd.to] > nd.cost + current.cost)) {
-                        current.visited[nd.to] = true;
+                    if (minCost[nd.to][nd.to] > nd.cost + current.cost) {
                         minCost[nd.to][nd.to] = nd.cost + current.cost;
-                        Next nst1 = new Next(nd.to, nd.to, nd.cost + current.cost, current.visited.clone());
-                        printNext("addNext", nst1);
-                        pQ.add(nst1);
-                        current.visited[nd.to] = false;
+                        pQ.add(new Next(nd.to, nd.to, nd.cost + current.cost));
                     }
                 }
+            } 
+            { // 따로 가는 경우
                 for (Node nd : ver[current.nowA].nodes) {
-                    if (!current.visited[nd.to]) {
-                        current.visited[nd.to] = true;
-                        if (minCost[current.nowA][nd.to] > nd.cost + current.cost) {
-                            minCost[current.nowA][nd.to] = nd.cost + current.cost;
-                            Next nst = new Next(current.nowA, nd.to, nd.cost + current.cost, current.visited.clone());
-                            printNext("addNext", nst);
-                            pQ.add(nst);
-                        }
-
-                        if (minCost[nd.to][current.nowB] > nd.cost + current.cost) {
-                            minCost[nd.to][current.nowB] = nd.cost + current.cost;
-                            Next nst2 = new Next(nd.to, current.nowB, nd.cost + current.cost, current.visited.clone());
-                            printNext("addNext", nst2);
-                            pQ.add(nst2);
-                        }
-
-                        current.visited[nd.to] = false;
+                    if(nd.to == current.nowB) {
+                        continue;
                     }
-                }
-            } else { // 따로 가는 경우
-                for (Node nd : ver[current.nowA].nodes) {
-                    if (!current.visited[nd.to] && (minCost[nd.to][current.nowB] > nd.cost + current.cost)) {
-                        current.visited[nd.to] = true;
+                    if ((minCost[nd.to][current.nowB] > nd.cost + current.cost)) {
                         minCost[nd.to][current.nowB] = nd.cost + current.cost;
-                        Next nst3 = new Next(nd.to, current.nowB, nd.cost + current.cost, current.visited.clone());
-                        printNext("addNext", nst3);
-                        pQ.add(nst3);
-                        current.visited[nd.to] = false;
+                        pQ.add(new Next(nd.to, current.nowB, nd.cost + current.cost));
                     }
                 }
                 for (Node nd : ver[current.nowB].nodes) {
-                    if (!current.visited[nd.to] && (minCost[current.nowA][nd.to] > nd.cost + current.cost)) {
-                        current.visited[nd.to] = true;
+                    if(nd.to == current.nowA) {
+                        continue;
+                    }
+                    if ((minCost[current.nowA][nd.to] > nd.cost + current.cost)) {
                         minCost[current.nowA][nd.to] = nd.cost + current.cost;
-                        Next nst4 = new Next(current.nowA, nd.to, nd.cost + current.cost, current.visited.clone());
-                        printNext("addNext", nst4);
-                        pQ.add(nst4);
-                        current.visited[nd.to] = false;
+                        pQ.add(new Next(current.nowA, nd.to, nd.cost + current.cost));
                     }
                 }
             }
         }
-
         return 0;
     }
 
-    void printNext(String add , Next n) {
-        if(add.contains("hand"))
+    void printNext(String add, Next n) {
+        if (add.contains("hand"))
             System.out.println(add + n);
     }
 
@@ -123,28 +92,15 @@ class Solution {
         int nowA;
         int nowB;
         int cost;
-        boolean[] visited;
 
-        public Next(int nowA, int nowB, int cost, boolean[] visited) {
+        public Next(int nowA, int nowB, int cost) {
             this.nowA = nowA;
             this.nowB = nowB;
             this.cost = cost;
-            this.visited = visited;
-        }
-
-        @Override
-        public String toString() {
-            return nowA + "," + nowB + "," + cost + ": " + Arrays.toString(visited);
         }
     }
 
     class Vertex {
-        int id;
-
-        public Vertex(int id) {
-            this.id = id;
-        }
-
         ArrayList<Node> nodes = new ArrayList<Node>();
     }
 
