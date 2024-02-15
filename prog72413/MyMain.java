5e33ad54cfc1ff8b9c83025a2627048732b508b6
iaaboy <dk6781.lee@gmail.com>
2024-02-15 18:45:51 +0900

    합승 택시

--- prog72413/MyMain.java
+++ prog72413/MyMain.java
@@ -1,6 +1,8 @@
 package prog72413;
 
-import java.util.*;
+/* 합승 택시
+https://school.programmers.co.kr/learn/courses/30/lessons/72413 
+ */
 
 public class MyMain {
     public static void main(String[] args) {
@@ -17,100 +19,44 @@ public class MyMain {
                         { 2, 5, 12 }, { 5, 3, 20 }, { 2, 4, 8 }, { 4, 3, 9 } }
         };
         Solution mSol = new Solution();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println("ans: " + mSol.solution(n[i], s[i], a[i], b[i], fares[i]));
         }
     }
 }
 
 class Solution {
+    static int INF = 2000000;
+
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        Vertex[] ver = new Vertex[n + 1];
-        int[][] minCost = new int[n + 1][n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            Arrays.fill(minCost[i], Integer.MAX_VALUE);
+        int[][] costFlo = new int[n + 1][n + 1];
+        for (int from = 1; from <= n; from++) {
+            for (int to = 1; to <= n; to++) {
+                if (from != to)
+                    costFlo[from][to] = INF;
+            }
         }
-        for (int i = 1; i < n + 1; i++) {
-            ver[i] = new Vertex();
-        }
+
         for (int[] fare : fares) {
-            if(fare[1] != s)
-                ver[fare[0]].nodes.add(new Node(fare[1], fare[2]));
-            if(fare[0] != s)
-                ver[fare[1]].nodes.add(new Node(fare[0], fare[2]));
+            costFlo[fare[0]][fare[1]] = fare[2];
+            costFlo[fare[1]][fare[0]] = fare[2];
         }
 
-        PriorityQueue<Next> pQ = new PriorityQueue<>((me, you) -> me.cost - you.cost);
-        pQ.add(new Next(s, s, 0, true));
-
-        while (!pQ.isEmpty()) {
-            Next current = pQ.poll();
-            // printNext("handle", current);
-            if (current.nowA == a && current.nowB == b) {
-                return current.cost;
-            }
-
-            if (current.isShare) { // 다음도 합승
-                for (Node nd : ver[current.nowA].nodes) {
-                    if (minCost[nd.to][nd.to] > nd.cost + current.cost) {
-                        minCost[nd.to][nd.to] = nd.cost + current.cost;
-                        pQ.add(new Next(nd.to, nd.to, nd.cost + current.cost, true));
-                    }
-                }
-            } 
-            {
-                for (Node nd : ver[current.nowA].nodes) {
-                    if ((minCost[nd.to][current.nowB] > nd.cost + current.cost)) {
-                        minCost[nd.to][current.nowB] = nd.cost + current.cost;
-                        pQ.add(new Next(nd.to, current.nowB, nd.cost + current.cost, false));
-                    }
-                }
-                for (Node nd : ver[current.nowB].nodes) {
-                    if ((minCost[current.nowA][nd.to] > nd.cost + current.cost)) {
-                        minCost[current.nowA][nd.to] = nd.cost + current.cost;
-                        pQ.add(new Next(current.nowA, nd.to, nd.cost + current.cost, false));
-                    }
+        for (int stop = 1; stop <= n; stop++) {
+            for (int from = 1; from <= n; from++) {
+                for (int to = 1; to <= n; to++) {
+                    if ((costFlo[from][stop] != INF && costFlo[stop][to] != INF))
+                        costFlo[from][to] = Math.min(costFlo[from][to], costFlo[from][stop] + costFlo[stop][to]);
                 }
             }
         }
-        return 0;
-    }
 
-    void printNext(String add, Next n) {
-        // if (add.contains("hand"))
-        //     System.out.println(add + n);
-    }
-
-    class Next {
-        int nowA;
-        int nowB;
-        int cost;
-        boolean isShare;
-
-        public Next(int nowA, int nowB, int cost, boolean isShare) {
-            this.nowA = nowA;
-            this.nowB = nowB;
-            this.cost = cost;
-            this.isShare = isShare;
+        int answer = Integer.MAX_VALUE;
+        answer = costFlo[s][b] + costFlo[s][a];
+        for (int mid = 1; mid <= n; mid++) {
+            if (costFlo[s][mid] != INF && costFlo[mid][a] != INF && costFlo[mid][b] != INF)
+                answer = Math.min(answer, costFlo[s][mid] + costFlo[mid][a] + costFlo[mid][b]);
         }
-
-        @Override
-        public String toString() {
-            return nowA +"," + nowB + ":" + cost;
-        }
-    }
-
-    class Vertex {
-        ArrayList<Node> nodes = new ArrayList<Node>();
-    }
-
-    class Node {
-        int to;
-        int cost;
-
-        public Node(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
-        }
+        return answer;
     }
 }
