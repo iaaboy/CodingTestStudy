4b358b043dd44a75c2ddf3d3dfddc93683a2f496
iaaboy <dk6781.lee@gmail.com>
2024-02-19 23:57:01 +0900

    멀쩡한 사각형 풀이

--- prog60059/MyMain.java
+++ prog60059/MyMain.java
@@ -8,144 +8,94 @@ import java.util.Arrays;
 
 public class MyMain {
     public static void main(String[] args) {
-        // int[][] key = {
-        // { 0, 0, 1 },
-        // { 0, 1, 0 },
-        // { 0, 1, 1 } };
-        int[][] key = { 
-            { 0, 0, 0 }, 
-            { 1, 0, 0 },
-            { 0, 1, 1 }};
+        int[][] key = {
+                { 0, 0, 0 },
+                { 1, 0, 0 },
+                { 0, 1, 1 }
+        };
         int[][] lock = {
                 { 1, 1, 1 },
                 { 1, 1, 0 },
-                { 1, 0, 1 } };
+                { 1, 0, 1 }
+        };
         Solution mSol = new Solution();
         System.out.println(mSol.solution(key, lock));
     }
 }
 
 class Solution {
-    int n;
-    int nMask;
+    int kMax;
+    int lMax;
 
     public boolean solution(int[][] key, int[][] lock) {
-        n = key.length;
-        int[] keyInt = new int[n];
-        int[] lockInt = new int[n];
-        for (int i = 0; i < n; i++) {
-            nMask |= (1 << i);
-        }
+        lMax = lock.length;
+        kMax = key.length;
+        int[][] keyExpand = new int[kMax + 2 * (lMax - 1)][kMax + 2 * (lMax - 1)];
 
-        lockInt = toInt(lock);
-        printArray("lock ", lockInt, true);
-        for (int i = 0; i < 3; i++) {
-            keyInt = rotate(key, i);
-            if (matchKeyUpDown(lockInt, keyInt))
-                return true;
-        }
-        return false;
-    }
-
-    private boolean matchKeyUpDown(int[] lockInt, int[] keyInt) {
-        boolean result;
-        printArray("key", keyInt, true);
-        Loop1: // 아래로 shift
-        for (int nShift = 0; nShift < n; nShift++) {
-            result = true;
-            for (int i = 0; i < nShift; i++) { // 락만 비교
-                if (((~lockInt[i] & nMask) != 0)) {
-                    result = false;
-                    continue Loop1;
+        for (int i = 0; i <= 3; i++) {
+            int[][] keyRotated = rotate(key, i);
+            for (int j = 0; j < keyExpand.length; j++) {
+                Arrays.fill(keyExpand[j], -1);
+            }
+            for (int y = lMax - 1; y < keyExpand.length - (lMax - 1); y++) {
+                for (int x = (lMax - 1); x < keyExpand.length - (lMax - 1); x++) {
+                    keyExpand[y][x] = keyRotated[y - (lMax - 1)][x - (lMax - 1)];
                 }
             }
-            for (int i = nShift; i < n; i++) { // 락 & 키 비교
-                if (((lockInt[i] & nMask) != (~keyInt[i - nShift] & nMask))) {
-                    result = false;
-                    continue Loop1;
-                }
-            }
-            if (result) {
-                return true;
-            }
-        }
-        Loop2: // 위로 shift
-        for (int nShift = 0; nShift < n; nShift++) {
-            result = true;
-            for (int i = 0; i < nShift; i++) { // 락 & 키 비교
-                if (((lockInt[i] & nMask) != (~keyInt[i + nShift] & nMask))) {
-                    result = false;
-                    continue Loop2;
-                }
-            }
-            for (int i = nShift; i < n; i++) { // 락만 비교
-                if (((~lockInt[i] & nMask) != 0)) {
-                    result = false;
-                    continue Loop2;
-                }
-            }
-            if (result) {
-                return true;
-            }
-        }
-        Loop3: // 좌로 shift
-        for (int nShift = 0; nShift < n; nShift++) {
-            result = true;
-            for (int i = 0; i < n; i++) { // 락만 비교
-                if (((lockInt[i] & nMask) != (~keyInt[i] << nShift & nMask))) {
-                    result = false;
-                    continue Loop3;
-                }
-            }
-            if (result) {
-                return true;
-            }
-        }
-        Loop4: // 좌로 shift
-        for (int nShift = 0; nShift < n; nShift++) {
-            result = true;
-            for (int i = 0; i < n; i++) { // 락만 비교
-                if (((lockInt[i] & nMask) != (~keyInt[i] >> nShift & nMask))) {
-                    result = false;
-                    continue Loop4;
-                }
-            }
-            if (result) {
+            printArray(i + ":", keyExpand);
+            if (matchAllKey(keyExpand, lock)) {
                 return true;
             }
         }
         return false;
     }
 
-    void printArray(String msg, int[] arr, boolean hex) {
-        if (hex) {
-            for (int i = 0; i < n; i++) {
-                System.out.println(Integer.toBinaryString(arr[i]));
+    private boolean matchAllKey(int[][] keyExpand, int[][] lock) {
+        for (int y = 0; y <= keyExpand.length - lock.length; y++) {
+            for (int x = 0; x <= keyExpand[0].length - lock[0].length; x++) {
+                if (checklock(keyExpand, y, x, lock)) {
+                    return true;
+                }
             }
-        } else
-            System.out.println(msg + Arrays.toString(arr));
+        }
+        return false;
     }
 
-    private int[] rotate(int[][] from, int rCount) {
-        int[][] rotated = rCount == 0 ? from : new int[n][n]; // rCount 0 이면 rotate안하고, from 바로 사용
+    private boolean checklock(int[][] keyExpand, int sY, int sX, int[][] lock) {
+        for (int y = sY; y < lock.length + sY; y++) {
+            for (int x = sX; x < lock.length + sX; x++) {
+                if (keyExpand[y][x] == -1 && lock[y - sY][x - sX] == 1) {
+
+                } else if (keyExpand[y][x] != -1 && lock[y - sY][x - sX] != keyExpand[y][x]) {
+
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void printArray(String string, int[][] keyExpand) {
+        System.out.println(string);
+        for (int i = 0; i < keyExpand.length; i++) {
+            System.out.println(Arrays.toString(keyExpand[i]));
+        }
+    }
+
+    private int[][] rotate(int[][] from, int rCount) {
+        if (rCount == 0)
+            return from;
+        int[][] rotated = new int[kMax][kMax];
         for (int i = 0; i < rCount; i++) {
-            for (int y = 0; y < n; y++) {
-                for (int x = 0; x < n; x++) {
-                    rotated[x][n - y - 1] = from[y][x];
+            rotated = new int[kMax][kMax];
+            for (int y = 0; y < kMax; y++) {
+                for (int x = 0; x < kMax; x++) {
+                    rotated[x][kMax - y - 1] = from[y][x];
                 }
             }
             from = rotated;
         }
-        return toInt(rotated);
-    }
-
-    private int[] toInt(int[][] arr) {
-        int[] result = new int[n];
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                result[y] |= (arr[y][x] << (n - x - 1));
-            }
-        }
-        return result;
+        return rotated;
     }
 }
