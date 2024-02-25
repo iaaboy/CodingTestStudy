5f4f3ce44f87b7749112c393aee5974bfa5c0810
iaaboy <dk6781.lee@gmail.com>
2024-02-25 14:51:26 +0900

    금과 은 운반하기 풀이

--- prog86053/MyMain.java
+++ prog86053/MyMain.java
@@ -1,7 +1,5 @@
 package prog86053;
 
-import java.util.PriorityQueue;
-
 /* 금과 은 운반하기
  * https://school.programmers.co.kr/learn/courses/30/lessons/86053
  */
@@ -9,11 +7,11 @@ import java.util.PriorityQueue;
 public class MyMain {
     public static void main(String[] args) {
         int[] a = { 90 };
-        int[] b = { 70 };
-        int[][] g = { { 70, 20, 0 } };
-        int[][] s = { { 40, 9, 0 } };
-        int[][] w = { { 34, 30, 2 } };
-        int[][] t = { { 4, 20, 1 } };
+        int[] b = { 500 };
+        int[][] g = { { 70, 70, 0 } };
+        int[][] s = { { 0, 0, 500 } };
+        int[][] w = { { 100, 100, 2 } };
+        int[][] t = { { 4, 8, 1 } };
 
         Solution mSol = new Solution();
         for (int i = 0; i < 1; i++) {
@@ -24,72 +22,46 @@ public class MyMain {
 
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        PriorityQueue<Work> workQ = new PriorityQueue<>((me, you) -> {
-            if (me.e == you.e) {
-                return t[me.id] - t[you.id];
+        long left = 0;
+        long right = 1000000000000001L;
+        long time;
+        while (true) {
+            time = left + ((right - left) / (long)2);
+            // System.out.println(time);
+            if (checkT(time, a, b, g, s, w, t)) {
+                right = time;
             } else {
-                if (me.e - you.e > 0) {
-                    return 1;
-                } else {
-                    return -1;
-                }
+                left = time;
             }
-        });
-
-        for (int i = 0; i < w.length; i++) {
-            if (s[i] > 0 || g[i] > 0) {
-                workQ.add(new Work(t[i], i));
+            if (left + 1 == right) {
+                break;
             }
         }
-
-        long latestHandled = 0;
-        while (!workQ.isEmpty()) {
-            Work cWork = workQ.poll();
-            System.out.println(a + ", " + b + " .. " + cWork);
-            // update loads
-            int load = w[cWork.id];
-            latestHandled = cWork.e;
-
-            if (g[cWork.id] > 0) {
-                int goldLoad = Math.min(Math.min(g[cWork.id], load), a);
-                load -= goldLoad;
-                g[cWork.id] -= goldLoad;
-                a -= goldLoad;
-            }
-
-            if (s[cWork.id] > 0) {
-                int silverLoad = Math.min(Math.min(s[cWork.id], load), b);
-                s[cWork.id] -= silverLoad;
-                load -= silverLoad;
-                b -= silverLoad;
-            }
-
-            // check finish condition
-            if (a <= 0 && b <= 0) {
-                return cWork.e;
-            }
-
-            // enqueue
-            if (s[cWork.id] > 0 || g[cWork.id] > 0) {
-                workQ.add(new Work(cWork.e + t[cWork.id] + t[cWork.id], cWork.id));
-            }
-        }
-
-        return latestHandled;
+        return right;
     }
 
-    class Work {
-        long e;
-        int id;
-
-        public Work(long e, int id) {
-            this.e = e;
-            this.id = id;
+    private boolean checkT(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
+        int n = g.length;
+        long gMax = 0L;
+        long sMax = 0L;
+        long wSum = 0L;
+        for (int i = 0; i < n; i++) {
+            long wCount = (time + t[i]) / (t[i] * 2L);
+            //해당 시간에 옮길 수 있는 무게
+            long wPossible = Math.min(wCount * w[i], g[i] + s[i]);
+            //최대 work
+            wSum += wPossible;
+            //i의 골드 최대
+            long gPossible = Math.min(wPossible, g[i]);
+             gMax += gPossible;// > 0 ? gPossible : 0;
+            //i의 실버 최대
+            long sPossible = Math.min(wPossible, s[i]);
+            sMax += sPossible;// > 0 ? sPossible : 0;
         }
-
-        @Override
-        public String toString() {
-            return id + ":" + e;
+        if (wSum >= a + b && gMax >= a && sMax >= b) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
