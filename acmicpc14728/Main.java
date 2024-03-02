602fc4a4aef5f5608e1be9dfe64b6b4d27354ebe
iaaboy <dk6781.lee@gmail.com>
2024-03-02 12:44:04 +0900

    벼락치기

--- acmicpc14728/Main.java
+++ acmicpc14728/Main.java
@@ -3,7 +3,7 @@ package acmicpc14728;
 import java.io.*;
 import java.util.StringTokenizer;
 
-/*
+/* 벼락치기
 https://www.acmicpc.net/problem/14728
 
 입력
@@ -16,16 +16,17 @@ https://www.acmicpc.net/problem/14728
 440
  */
 
-public class MaMain {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+public class Main {
+    public static void main(String[] args) throws NumberFormatException,
+            IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(st.nextToken());
 
-        int[] times = new int[n];
-        int[] scores = new int[n];
+        int[] times = new int[n + 1];
+        int[] scores = new int[n + 1];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int k = Integer.parseInt(st.nextToken());
@@ -33,22 +34,23 @@ public class MaMain {
             times[i] = k;
             scores[i] = s;
         }
-        new MaMain().solution(times, scores, t);
+        new Main().solution(times, scores, t);
     }
 
     public void solution(int[] times, int[] scores, int t) { // N 출력
         int[][] maxScores = new int[times.length + 1][t + 1];
 
-        for (int y = 0; y < times.length; y++) {
-            for (int x = 0; x < t; x++) {
-                if (x - scores[y] <= 0) {
+        for (int y = 1; y <= times.length; y++) {
+            for (int x = 0; x <= t; x++) {
+                if (x - times[y - 1] < 0) {
                     // 전에꺼 그대로
+                    maxScores[y][x] = maxScores[y - 1][x];
                 } else {
                     // 전에꺼 + 다음꺼
+                    maxScores[y][x] = Math.max(maxScores[y - 1][x], maxScores[y - 1][x - times[y - 1]] + scores[y - 1]);
                 }
             }
         }
-
         System.out.println(maxScores[times.length][t]);
     }
 }
