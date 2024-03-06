3e797901f40d2cd59c6fa20b2f1872e7d8ea63ce
iaaboy <dk6781.lee@gmail.com>
2024-03-07 00:24:45 +0900

    [1차] 뉴스 클러스터링 풀이

--- prog17677/MyMain.java
+++ prog17677/MyMain.java
@@ -2,16 +2,14 @@ package prog17677;
 
 import java.util.*;
 
+/* [1차] 뉴스 클러스터링
+ * https://school.programmers.co.kr/learn/courses/30/lessons/17677
+ */
+
 public class MyMain {
     public static void main(String[] args) {
-        // str1 str2 answer
-        // FRANCE french 16384
-        // handshake shake hands 65536
-        // aa1+aa2 AAAA12 43690
-        // E=M*C^2 e=m*c^2 65536
-
-        String[] str1 = { "FRANCE", "handshake", "aa1+aa2", "E=M*C^2" };
-        String[] str2 = { "french", "shake hands", "AAAA12", "e=m*c^2" };
+        String[] str1 = { "aaabbcc", "FRANCE", "handshake", "aa1+aa2", "E=M*C^2" };
+        String[] str2 = { "aabbcc", "french", "shake hands", "AAAA12", "e=m*c^2" };
         Solution mSol = new Solution();
         for (int i = 0; i < 4; i++) {
             System.out.println(mSol.solution(str1[i], str2[i]));
@@ -23,12 +21,12 @@ class Solution {
     public int solution(String str1, String str2) {
         List<String> str1List = new ArrayList<>();
         List<String> str2List = new ArrayList<>();
-
         str1 = str1.toLowerCase();
         char[] st1Arr = str1.toCharArray();
         str2 = str2.toLowerCase();
         char[] st2Arr = str2.toCharArray();
         List<String> strUnion = new ArrayList<>();
+        List<String> strRetain = new ArrayList<>();
 
         for (int i = 0; i < st1Arr.length - 1; i++) {
             if (isLetter(st1Arr[i]) && isLetter(st1Arr[i + 1])) {
@@ -40,13 +38,26 @@ class Solution {
                 str2List.add(str2.substring(i, i + 2));
             }
         }
+        for (String st : str1List) {
+            strUnion.add(st);
+        }
+        strUnion.addAll(str2List);
 
-        System.out.println(str1List);
-        System.out.println(str2List);
-        str1List.retainAll(str2List);
-        System.out.println(str1List);
+        for (String st : str1List) {
+            if (str2List.contains(st)) {
+                strRetain.add(st);
+                str2List.remove(st);
+            }
+        }
 
-        return 65536 * str1List.size() / strUnion.size();
+        // System.out.println(strUnion);
+        // System.out.println(strRetain);
+
+        if (strUnion.size() - strRetain.size() == 0) {
+            return 65536;
+        }
+
+        return 65536 * strRetain.size() / (strUnion.size() - strRetain.size());
     }
 
     private boolean isLetter(char c) {
