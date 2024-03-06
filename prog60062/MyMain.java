5840568233e2ff114bee3b9e362bec5faede15fc
iaaboy <dk6781.lee@gmail.com>
2024-03-07 01:40:56 +0900

    외벽 점검 중간

--- prog60062/MyMain.java
+++ prog60062/MyMain.java
@@ -2,6 +2,10 @@ package prog60062;
 
 import java.util.*;
 
+/* 외벽 점검
+ * https://school.programmers.co.kr/learn/courses/30/lessons/60062
+ */
+
 public class MyMain {
     public static void main(String[] args) {
         int[] n = { 12, 12 };
@@ -20,27 +24,63 @@ public class MyMain {
 }
 
 class Solution {
+    int[] extendedWeak;
     public int solution(int n, int[] weak, int[] dist) {
+        extendedWeak = new int[weak.length * 2];
 
-        // 1. dist 의 조합을 구한다.
+        // 0. 외곽을 확장
+        for(int i = 0 ; i < extendedWeak.length ; i++) {
+            if(i < weak.length) {
+                extendedWeak[i] = weak[i];
+            } else {
+                extendedWeak[i] = weak[i - weak.length] + n;
+            }
+        }
+
+        System.out.println(Arrays.toString(extendedWeak));
+
+        // 1. dist 의 순열을 구한다.
         // 2. 각 조합에서 커버 가능한지 확인한다.
-        int[] chosen = new int[dist.length];
-        combi(chosen, dist, 0, 0, 3);
+        for(int i = 1; i <= dist.length ; i++) {
+            permu(dist, 0, 2);
+        }
 
         int answer = 0;
         return answer;
     }
 
-    private void combi(int[] chosen, int[] dist, int index, int count, int targetCunt) {
-        if (count == targetCunt) {
-            for (int i = 0; i < count; i++)
-                System.out.print(chosen[i] + " ");
-            System.out.println();
+    private void permu(int[] dist, int count, int targetCnt) {
+        if (count == targetCnt) {
+            if(checkAllStartPoint(dist, count)) {
+                System.out.println("passed");
+            }
             return;
         }
-        for (int i = index; i < dist.length; i++) {
-            chosen[count] = dist[i];
-            combi(chosen, dist, i + 1, count + 1, targetCunt);
+        for (int i = count; i < dist.length; i++) {
+            swap(dist, i, count);
+            permu(dist, count + 1, targetCnt);
+            swap(dist, i, count);
         }
+    }
+
+    private boolean checkAllStartPoint(int[] dist, int count) {
+        int n = extendedWeak.length / 2 ;
+        for(int i = 0 ; i < n; i++) { // 시작점 변경
+            if(isPass(i, dist, count)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isPass(int i, int[] dist, int count) {
+
+        return false;
+    }
+
+    void swap(int[] dist, int from, int to) {
+        int temp = dist[from];
+        dist[from] = dist[to];
+        dist[to] = temp;
     }
 }
