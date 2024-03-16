d986b8c13a0cf4013b2aa351243ca443d7f26dc5
iaaboy <dk6781.lee@gmail.com>
2024-03-16 17:14:51 +0900

    양궁대회 풀이

--- prog92342/MyMain.java
+++ prog92342/MyMain.java
@@ -10,14 +10,14 @@ public class MyMain {
     public static void main(String[] args) {
         int[] n = { 5, 1, 9, 10 };
         int[][] infos = {
-                { 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, //[0,2,2,0,1,0,0,0,0,0,0]
-                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //[-1]
-                { 0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1 }, //[1,1,2,0,1,2,2,0,0,0,0]
-                { 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3 } //[1,1,1,1,1,1,1,1,0,0,2]
+                { 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, // [0,2,2,0,1,0,0,0,0,0,0]
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // [-1]
+                { 0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1 }, // [1,1,2,0,1,2,2,0,0,0,0]
+                { 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3 } // [1,1,1,1,1,1,1,1,0,0,2]
         };
 
         Solution mSol = new Solution();
-        for (int i = 0; i < infos.length; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println(Arrays.toString(mSol.solution(n[i], infos[i])));
         }
 
@@ -28,76 +28,74 @@ class Solution {
     int n;
     int[] info;
     int maxScore;
-    ArrayList<boolean[]> winList;
+    boolean[] maxWin;
 
     public int[] solution(int n, int[] info) {
         boolean[] win = new boolean[11];
-        winList = new ArrayList<>();
         this.n = n;
         this.info = info;
         maxScore = -1;
 
         dfs(win, 0);
 
-        // System.out.println("winList: " + winList);
-
-        int[] answer;
-        if (winList.size() == 0) {
-            answer = new int[] { -1 };
-        } else {// if(winList.size() > 0) {
-            answer = new int[info.length];
-            int remainedArrow = n;
-            for (int i = 0; i < n; i++) {
-                if (winList.get(0)[i]) {
-                    answer[i] = info[i] + 1;
-                    remainedArrow-=answer[i];
-                }
-            }
-            if(remainedArrow > 0) {
-                answer[answer.length - 1] = remainedArrow;
+        if (maxScore == -1) {
+            return new int[] { -1 };
+        }
+        int[] answer = new int[info.length];
+        int remainedArrow = n;
+        for (int i = 0; i < answer.length; i++) {
+            if (maxWin[i]) {
+                answer[i] = info[i] + 1;
+                remainedArrow -= answer[i];
             }
         }
-
+        if (remainedArrow > 0) {
+            answer[answer.length - 1] = remainedArrow;
+        }
         return answer;
     }
 
-    int count = 0;
-
     private void dfs(boolean[] win, int depth) {
         if (depth == win.length) {
-            // check 화살수
-            int score = checkCount(win);
+            int score = calcScore(win);
             if (score != -1) {
                 if (score == maxScore) {
                     maxScore = score;
-                    // System.out.println(count + ":" + score + " - " + Arrays.toString(win));
-                    count++;
-                    winList.add(win.clone());
+                    // System.out.println(score + " - " + Arrays.toString(win));
+                    maxWin = compareWins(maxWin, win.clone());
                 } else if (score > maxScore) {
                     maxScore = score;
-                    // System.out.println(count + ":" + score + " - " + Arrays.toString(win));
-                    count++;
-                    winList.clear();
-                    winList.add(win.clone());
+                    // System.out.println(score + " - " + Arrays.toString(win));
+                    maxWin = win.clone();
                 }
             }
             return;
         }
-
         win[depth] = true;
         dfs(win, depth + 1);
         win[depth] = false;
         dfs(win, depth + 1);
-
     }
 
-    private int checkCount(boolean[] win) {
-        int total = 0;
+    // 동점일 때 낮은 점수로 맞혔을 때 우선
+    private boolean[] compareWins(boolean[] yourWin, boolean[] myWin) {
+        for (int i = myWin.length - 1; i >= 0; i--) {
+            if (yourWin[i] && !myWin[i]) {
+                return yourWin;
+            } else if (!yourWin[i] && myWin[i]) {
+                return myWin;
+            }
+        }
+        return yourWin;
+    }
+
+    private int calcScore(boolean[] win) {
+        int totalArrow = 0;
         int lionSum = 0;
         int apeachSum = 0;
         for (int i = 0; i < win.length; i++) {
             if (win[i]) {
-                total += info[i] + 1;
+                totalArrow += info[i] + 1;
                 lionSum += win.length - 1 - i;
             } else {
                 if (info[i] > 0) {
@@ -105,8 +103,8 @@ class Solution {
                 }
             }
         }
-        if (apeachSum < lionSum && total <= n) {
-            return lionSum;
+        if (apeachSum < lionSum && totalArrow <= n) {
+            return lionSum - apeachSum; // 라이언과 어피치의 점수차!!
         } else {
             return -1;
         }
