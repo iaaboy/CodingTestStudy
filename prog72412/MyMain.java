eac52f50e3bbf829f50a9915c350ccd4c3e004a5
iaaboy <dk6781.lee@gmail.com>
2024-03-17 20:34:27 +0900

    순위 검색 또 실패

--- prog72412/MyMain.java
+++ prog72412/MyMain.java
@@ -35,152 +35,127 @@ public class MyMain {
 // Soul food [chicken, pizza]
 
 class Solution {
-    Record[][][][] mRecord = new Record[3 + 1][2 + 1][2 + 1][2 + 1];
-
     public int[] solution(String[] info, String[] query) {
-        int[] answer = new int[query.length];
-        for (int a = 0; a < 3; a++)
-            for (int b = 0; b < 2; b++)
-                for (int c = 0; c < 2; c++)
-                    for (int d = 0; d < 2; d++)
-                        mRecord[a][b][c][d] = new Record();
+        HashMap<String, Integer> keyMap = new HashMap<>();
+        keyMap.put("Ljcp", 0);
+        keyMap.put("Ljc", 1);
+        keyMap.put("Ljp", 2);
+        keyMap.put("Lcp", 3);
+        keyMap.put("Lj", 4);
+        keyMap.put("Lc", 5);
+        keyMap.put("Lp", 6);
+        keyMap.put("Jfb", 0);
+        keyMap.put("Jf", 1);
+        keyMap.put("Jb", 2);
+        keyMap.put("Csj", 0);
+        keyMap.put("Cs", 1);
+        keyMap.put("Cj", 2);
+        keyMap.put("Scp", 0);
+        keyMap.put("Sc", 1);
+        keyMap.put("Sp", 2);
 
-        for (String str : info) {
-            String[] s = str.split(" ");
-            int score = Integer.parseInt(s[4]);
-            int lang = 0;
-            if (s[0].charAt(0) == 'c') {
-                lang = 1;
-            } else if (s[0].charAt(0) == 'p') {
-                lang = 2;
+        String[] LangKeys = { "Ljcp", "Ljc", "Ljp", "Lcp", "Lj", "Lc", "Lp" };
+        String[] JobKeys = { "Jfb", "Jf", "Jb" };
+        String[] CarrKeys = { "Csj", "Cs", "Cj" };
+        String[] SoulFoodKeys = { "Scp", "Sc", "Sp" };
+
+        List<Integer>[][][][] scoreMap = new List[7][3][3][3];
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int j2 = 0; j2 < 3; j2++) {
+                    for (int k = 0; k < 3; k++) {
+                        scoreMap[i][j][j2][k] = new LinkedList<>();
+                    }
+                }
             }
-            int job = s[1].charAt(0) == 'f' ? 0 : 1;
-            int carrier = s[2].charAt(0) == 's' ? 0 : 1;
-            int soulFood = s[3].charAt(0) == 'c' ? 0 : 1;
-            setScore(lang, job, carrier, soulFood, score);
         }
 
-        for (int a = 0; a < 3; a++)
-            for (int b = 0; b < 2; b++)
-                for (int c = 0; c < 2; c++)
-                    for (int d = 0; d < 2; d++) {
-                        Collections.sort(mRecord[a][b][c][d].s);
-                        // System.out.println(a + "," + b + "," + c + "," + d + ":" + mRecord[a][b][c][d].s);
-                    }
+        for (int i = 0; i < info.length; i++) {
+            String[] member = info[i].split(" ");
+            char chLang = member[0].charAt(0);
+            char chjob = member[1].charAt(0);
+            char chCarreer = member[2].charAt(0);
+            char chSoulFood = member[3].charAt(0);
+            int score = Integer.parseInt(member[4]);
 
-        Set<Score> scores = new HashSet<>();
-        int answerIdx = 0;
-        for (String str : query) {
-            scores.clear();
-            str = str.replace("and ", "");
-            String[] s = str.split(" ");
-            int lang = 0;
-            if (s[0].charAt(0) == 'c') {
-                lang = 1;
-            } else if (s[0].charAt(0) == 'p') {
-                lang = 2;
-            } else if (s[0].charAt(0) == '-') {
-                lang = 10;
-            }
-            int job = s[1].charAt(0) == 'f' ? 0 : 1;
-            if (s[1].charAt(0) == '-') {
-                job = 10;
-            }
-            int carrier = s[2].charAt(0) == 's' ? 0 : 1;
-            if (s[2].charAt(0) == '-') {
-                carrier = 10;
-            }
-            int soulFood = s[3].charAt(0) == 'c' ? 0 : 1;
-            if (s[3].charAt(0) == '-') {
-                soulFood = 10;
-            }
-
-            for (int a = 0; a < 3; a++) {
-                for (int b = 0; b < 2; b++) {
-                    for (int c = 0; c < 2; c++) {
-                        for (int d = 0; d < 2; d++) {
-                            // System.out.println(a + "," + b + "," + c + "," + d);
-                            if ((a == lang || lang == 10) && (job == b || job == 10)
-                                    && (carrier == c || carrier == 10) && (soulFood == d || soulFood == 10)) {
-                                scores.addAll(mRecord[a][b][c][d].s);
+            for (String lang : LangKeys) {
+                if (hasChar(lang, chLang)) {
+                    for (String job : JobKeys) {
+                        if (hasChar(job, chjob)) {
+                            for (String carr : CarrKeys) {
+                                if (hasChar(carr, chCarreer)) {
+                                    for (String soulfood : SoulFoodKeys) {
+                                        if (hasChar(soulfood, chSoulFood)) {
+                                            scoreMap[keyMap.get(lang)][keyMap.get(job)][keyMap.get(carr)][keyMap
+                                                    .get(soulfood)].add(score);
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
-            Set <Integer> nums = new HashSet<>();
-            int num = Integer.parseInt(s[4]);
-
-            
-            for(Score sc : scores) {
-                if(sc.s < num) {
-                    break;
-                }
-                nums.add(sc.index);
-            }
-            answer[answerIdx++] = nums.size();
         }
 
-
-
-        return answer;
-    }
-
-    // Language [java, cpp, python ]
-    // Job [backend, frontend]
-    // Career [senior, junior]
-    // Soul food [chicken, pizza]
-    int scoreIndex = 1;
-
-    void setScore(int lang, int job, int career, int soufFood, int score) {
-        for (int a = 0; a < 3; a++) {
-            for (int b = 0; b < 2; b++) {
-                for (int c = 0; c < 2; c++) {
-                    for (int d = 0; d < 2; d++) {
-                        // System.out.println(a + "," + b + "," + c + "," + d);
-                        if (a == lang && job == b && career == c && soufFood == d) {
-                            mRecord[a][b][c][d].s.add(new Score(scoreIndex++, score));
-                        }
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int j2 = 0; j2 < 3; j2++) {
+                    for (int k = 0; k < 3; k++) {
+                        scoreMap[i][j][j2][k].sort(null);
+                        // if (scoreMap[i][j][j2][k].size() > 0)
+                        // System.out.println(i + "," + j + "," + j2 + "," + k + "," +
+                        // scoreMap[i][j][j2][k]);
                     }
                 }
             }
         }
-    }
-}
 
-class Record {
-    Vector<Score> s = new Vector<>();
-}
+        int[] answer = new int[query.length];
+        int index = 0;
+        for (String q : query) {
+            String[] qSepa = q.split(" ");
+            String keyLang = "L" + (qSepa[0].charAt(0) == '-' ? "jcp" : qSepa[0].charAt(0));
+            String keyJob = "J" + (qSepa[2].charAt(0) == '-' ? "fb" : qSepa[2].charAt(0));
+            String keyCarr = "C" + (qSepa[4].charAt(0) == '-' ? "sj" : qSepa[4].charAt(0));
+            String keySoulFood = "S" + (qSepa[6].charAt(0) == '-' ? "cp" : qSepa[6].charAt(0));
+            Integer targetScore = Integer.parseInt(qSepa[7]);
 
-class Score implements Comparable<Score> {
-    int index;
-    int s;
-
-    public Score(int index, int s) {
-        this.index = index;
-        this.s = s;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return index == ((Score) obj).index;
-    }
-
-    @Override
-    public int compareTo(Score o) {
-        if (s == o.s) {
-            return index - o.index;
+            // System.out.println(keyLang + " " + keyJob + " " + keyCarr + " " + keySoulFood
+            // + " " + targetScore);
+            List<Integer> scoreList = scoreMap[keyMap.get(keyLang)][keyMap.get(keyJob)][keyMap.get(keyCarr)][keyMap
+                    .get(keySoulFood)];
+            int ub = lowerBound(scoreList, targetScore);
+            // System.out.println(targetScore + ": " + ub + " ----> " + scoreList);
+            answer[index] = scoreList.size() - ub;
+            index++;
         }
-        return s - o.s;
+
+        return answer;
     }
 
-    @Override
-    public int hashCode() {
-        return index;
+    private static int lowerBound(List<Integer> arr, int target) {
+        int start = 0;
+        int end = arr.size();
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (arr.get(mid) >= target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
     }
 
-    @Override
-    public String toString() {
-        return index + "," + s;
+    private boolean hasChar(String string, char lang) {
+        for (char ch : string.toCharArray()) {
+            if (ch == lang) {
+                return true;
+            }
+        }
+        return false;
     }
 }
