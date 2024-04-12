f36d55df5157767e53541e95dd917ffad519477e
iaaboy <dk6781.lee@gmail.com>
2024-04-12 22:16:26 +0900

    N-Queen 풀이.. 어렵다.. 답 참고.

--- prog12952/MyMain.java
+++ prog12952/MyMain.java
@@ -1,18 +1,60 @@
 package prog12952;
 
+import java.util.Arrays;
+
+/* N-Queen 풀이
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12952
+ */
 public class MyMain {
     public static void main(String[] args) {
         Solution mSol = new Solution();
-        System.out.println(mSol.solution(4));
+        for (int i = 4; i < 5; i++) {
+            System.out.println(mSol.solution(i));
+        }
+
     }
 }
 
 class Solution {
+    int n;
+    int board[][];
+    int answer;
+
     public int solution(int n) {
+        this.n = n;
+        board = new int[n][n];
+        answer = 0;
 
-        
+        dfs(0);
 
-        int answer = 0;
         return answer;
+    }
+
+    private void dfs(int depth) {
+        // System.out.println(depth + "," + Arrays.toString(board));
+        if (depth == n) {
+            answer++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            board[depth] = i;
+
+            boolean hasNext = true;
+            for (int j = 0; j < depth; j++) {
+                if (board[depth] == board[j]) { //직선
+                    hasNext = false;
+                    break;
+                }
+                if (Math.abs(depth - j) == Math.abs(board[depth] - board[j])) { //대각선
+                    hasNext = false;
+                    break;
+                }
+            }
+
+            if (hasNext) {
+                dfs(depth + 1);
+            }
+        }
     }
 }
