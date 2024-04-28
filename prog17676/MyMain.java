9413ae1572a594e88f27f24d8c5c844cb7686277
iaaboy <dk6781.lee@gmail.com>
2024-04-28 10:15:46 +0900

    [1차] 추석 트래픽 풀이

--- prog17676/MyMain.java
+++ prog17676/MyMain.java
@@ -1,7 +1,6 @@
 package prog17676;
 
 import java.time.*;
-import java.util.*;
 
 /* [1차] 추석 트래픽
  * https://school.programmers.co.kr/learn/courses/30/lessons/17676
@@ -10,12 +9,24 @@ import java.util.*;
 public class MyMain {
     public static void main(String[] args) {
         String[][] lines = {
+                { "2016-09-15 23:59:59.999 0.001s" },
                 { "2016-09-15 01:00:04.001 2.0s",
-                        "2016-09-15 01:00:04.001 2.0s", },
-
+                        "2016-09-15 01:00:07.000 2s" },
+                { "2016-09-15 01:00:04.002 2.0s",
+                        "2016-09-15 01:00:07.000 2s" },
+                { "2016-09-15 20:59:57.421 0.351s",
+                        "2016-09-15 20:59:58.233 1.181s",
+                        "2016-09-15 20:59:58.299 0.8s",
+                        "2016-09-15 20:59:58.688 1.041s",
+                        "2016-09-15 20:59:59.591 1.412s",
+                        "2016-09-15 21:00:00.464 1.466s",
+                        "2016-09-15 21:00:00.741 1.581s",
+                        "2016-09-15 21:00:00.748 2.31s",
+                        "2016-09-15 21:00:00.966 0.381s",
+                        "2016-09-15 21:00:02.066 2.62s" }
         };
         Solution mSol = new Solution();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < lines.length; i++) {
             System.out.println(mSol.solution(lines[i]));
         }
     }
@@ -41,99 +52,59 @@ class Solution {
                 processTime = Integer.parseInt(processT) * ((int) Math.pow(10, 4 - processT.length()));
             }
             processTime--;
-            long end = replyTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli() - 1473868800000l;
+            long end = replyTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();// - 1473868800000l;
             long start = end - processTime;
             // System.out.println(tToStr(start) + " -> " + tToStr(end));
             traffics[index++] = new TR(start, end);
         }
 
-        Integer[] trStartAligned = new Integer[n];
-        Integer[] trEndAligned = new Integer[n];
+        Integer[] trStartIdx = new Integer[n];
+        Integer[] trEndIdx = new Integer[n];
         for (int i = 0; i < n; i++) {
-            trEndAligned[i] = i;
-            trStartAligned[i] = i;
+            trEndIdx[i] = i;
+            trStartIdx[i] = i;
         }
 
-        Arrays.sort(trStartAligned, (a, b) -> {
-            if (traffics[a].start > traffics[b].start) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
-        Arrays.sort(trEndAligned, (a, b) -> {
-            if (traffics[a].end > traffics[b].end) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
-
-        System.out.println("start");
-        for (int i = 0; i < trStartAligned.length; i++) {
-            System.out.print(traffics[trStartAligned[i]].start + ", ");
-        }
-        System.out.println("\nend");
-        for (int i = 0; i < trEndAligned.length; i++) {
-            System.out.print(traffics[trEndAligned[i]].end + ", ");
-        }
-        System.out.println();
-
-        // 시작시간 기준 push하고,
-        // 종료시간 기준 pQ에 넣어 처리
-        PriorityQueue<Integer> pQ = new PriorityQueue<>((a, b) -> {
-            if (traffics[a].end > traffics[b].end) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
+        // System.out.println("start");
+        // for (int i = 0; i < trStartIdx.length; i++) {
+        // System.out.print(traffics[trStartIdx[i]].start + ", ");
+        // }
+        // System.out.println("\nend");
+        // for (int i = 0; i < trEndIdx.length; i++) {
+        // System.out.print(traffics[trEndIdx[i]].end + ", ");
+        // }
+        // System.out.println();
 
         int answer = 0;
-        int startIndex = 0;
-        for (int i = 0; i < trEndAligned.length; i++) {
-            // System.out.println();
-            long curTime = traffics[trEndAligned[i]].end;
-            // q push
-            for (int j = startIndex; j < trStartAligned.length; j++) {
-                if (curTime > traffics[trStartAligned[j]].start) {
-                    System.out.print(curTime + " -> push: " + traffics[trStartAligned[j]].end +
-                            "/"
-                            + traffics[trStartAligned[j]].end + "\n");
-                    pQ.add(trStartAligned[j]);
-                    startIndex = j;
-                } else {
-                    startIndex = j;
-                    break;
-                }
-            }
-            // 종료된 것 처리
-            while (!pQ.isEmpty() && curTime > traffics[pQ.peek()].end) {
-                System.out
-                        .print(curTime + " -> poll: " + traffics[pQ.peek()].end + "/" +
-                                traffics[pQ.peek()].end + "\n");
-                pQ.poll();
-            }
 
-            // 남은 것 count
+        // 시작 ~ 끝
+        for (int i = 0; i < n; i++) {
+            long startRange = traffics[trStartIdx[i]].start;
+            long endRange = startRange + 999;
+
             int count = 0;
-            for (int j = startIndex; j < trStartAligned.length; j++) {
-                if (i == j)
-                    continue;
-                if (curTime + 1000 > traffics[trStartAligned[j]].start) {
-                    System.out.println("check: " + (curTime + 1000) + " vs " +
-                            traffics[trStartAligned[j]].start);
+            for (int j = 0; j < n; j++) {
+                if (traffics[j].start <= endRange && traffics[j].end >= startRange) {
                     count++;
-                } else {
-                    System.out.println("pass : " + (curTime + 1000) + " vs " +
-                            traffics[trStartAligned[j]].start);
-                    break;
                 }
             }
-            answer = Math.max(answer, count + pQ.size());
-            System.out
-                    .println("count: " + count + " , qSize: " + pQ.size() + " , " +
-                            traffics[trStartAligned[i]].start);
+            answer = Math.max(answer, count);
+            // System.out.println(startRange + ": " + count);
+        }
+
+        // 시작 ~ 끝
+        for (int i = 0; i < n; i++) {
+            long startRange = traffics[trEndIdx[i]].end;
+            long endRange = startRange + 999;
+
+            int count = 0;
+            for (int j = 0; j < lines.length; j++) {
+                if (traffics[j].start <= endRange && traffics[j].end >= startRange) {
+                    count++;
+                }
+            }
+            answer = Math.max(answer, count);
+            // System.out.println(startRange + ": " + count);
         }
 
         return answer;
