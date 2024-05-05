eabd8af9197f61748209316e2846e0a4777ce1b9
iaaboy <dk6781.lee@gmail.com>
2024-05-05 11:35:14 +0900

    명예의 전당 (1) 풀이

--- progLevel1/MyMain.java
+++ progLevel1/MyMain.java
@@ -3,46 +3,39 @@ package progLevel1;
 import java.util.*;
 
 /*
- * https://school.programmers.co.kr/learn/courses/30/lessons/17681
+ * https://school.programmers.co.kr/learn/courses/30/lessons/138477
  */
 
 public class MyMain {
     public static void main(String[] args) {
-        int n = 6;
-        int[] arr = { 46, 33, 33, 22, 31, 50 };
-        int[] arr2 = { 27, 56, 19, 14, 14, 10 };
+        int k = 4;
+        int[] score = { 0, 300, 40, 300, 20, 70, 150, 50, 500, 1000 };
         Solution mSol = new Solution();
 
-        System.out.println(Arrays.toString(mSol.solution(n, arr, arr2)));
+        System.out.println(Arrays.toString(mSol.solution(k, score)));
     }
 }
 
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] var = new String[n];
-        String[] answer = new String[n];
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> topkScore = new PriorityQueue<>();
+        int[] answer = new int[score.length];
 
-        for (int i = 0; i < var.length; i++)
-            var[i] = "";
-
-        for (int i = 0; i < arr1.length; i++) {
-            int c = arr1[i] | arr2[i];
-            String cs = Integer.toBinaryString(c);
-            StringBuffer sb = new StringBuffer();
-            if (cs.length() < n) {
-                for (int j = 0; j < n - cs.length(); j++) {
-                    sb.append(" ");
-                }
-            }
-            for (int j = 0; j < cs.length(); j++) {
-                if (cs.charAt(j) == '1') {
-                    sb.append("#");
+        for (int i = 0; i < score.length; i++) {
+            if (k > 0) {
+                topkScore.add(score[i]);
+                k--;
+            } else {
+                if (topkScore.peek() >= score[i]) {
+                    // skip
                 } else {
-                    sb.append(" ");
+                    topkScore.poll();
+                    topkScore.add(score[i]);
                 }
             }
-            answer[i] = sb.toString();
+            answer[i] = topkScore.peek();
         }
+
         return answer;
     }
 }
