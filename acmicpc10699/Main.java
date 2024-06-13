14f6eaea013153005194aea9c86e2cd8e3b31841
iaaboy <dk6781.lee@gmail.com>
2024-06-14 02:55:04 +0900

    새싹풀이

---
+++ acmicpc10699/Main.java
@@ -0,0 +1,17 @@
+package acmicpc10699;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] catarr = {
+                "         ,r'\"7",
+                "r`-_   ,'  ,/",
+                " \\. \". L_r'",
+                "   `~\\/",
+                "      |",
+                "      |",
+        };
+        for (int i = 0; i < catarr.length; i++) {
+            System.out.println(catarr[i]);
+        }
+    }
+}
