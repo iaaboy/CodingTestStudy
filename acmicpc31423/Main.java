a69b58e8e197bcb2c508f7415d398a55ccee2844
iaaboy <dk6781.lee@gmail.com>
2024-09-14 08:41:04 +0900

    신촌 통폐합 계획 풀이

--- acmicpc31423/Main.java
+++ acmicpc31423/Main.java
@@ -2,51 +2,36 @@ package acmicpc31423;
 
 import java.io.*;
 import java.util.*;
-
+/* 신촌 통폐합 계획
+ * https://www.acmicpc.net/problem/31423 
+ */
 public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
         String[] names = new String[N];
-        int[] head = new int[N + 1];
+        int[] next = new int[N + 1];
         int[] tail = new int[N + 1];
         for (int i = 0; i < N; i++) {
             names[i] = bf.readLine();
+            next[i + 1] = i + 1;
+            tail[i + 1] = i + 1;
         }
+        int startIdx = 0;
         for (int i = 0; i < N - 1; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-
-            // set to Head
-            int headIdx = to;
-            while (head[headIdx] != 0) {
-                headIdx = head[headIdx];
-            }
-            head[headIdx] = from;
-
-            // set to Tail
-            int tailIdx = from;
-            while (tail[tailIdx] != 0) {
-                tailIdx = tail[tailIdx];
-            }
-            tail[tailIdx] = to;
+            next[tail[from]] = to;
+            tail[from] = tail[to];
+            startIdx = from;
         }
-        int startIdx = 0;
-        for (int i = 0; i < N; i++) {
-            if (head[i] == 0) {
-                startIdx = i;
-            }
-        }
-        // System.out.println(Arrays.toString(head));
-        // System.out.println(Arrays.toString(tail));
 
         StringBuilder sb = new StringBuilder();
-        do {
+        for (int i = 0; i < N; i++) {
             sb.append(names[startIdx - 1]);
-            startIdx = tail[startIdx];
-        } while (startIdx != 0);
-
+            startIdx = next[startIdx];
+        }
         System.out.println(sb);
     }
 }
