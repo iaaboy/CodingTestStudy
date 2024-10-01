ba9a033ed89b62bc399dd6dbd1e9a2ad338a985e
iaaboy <dk6781.lee@gmail.com>
2024-10-02 00:48:23 +0900

    최소 스패닝 트리 풀이

---
+++ acmicpc1197/Main.java
@@ -0,0 +1,87 @@
+package acmicpc1197;
+
+import java.io.*;
+import java.util.*;
+
+/* 최소 스패닝 트리
+ * https://www.acmicpc.net/problem/1197
+ */
+
+public class Main {
+    static int[] ids;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        ids = new int[V + 1];
+        for (int i = 1; i < ids.length; i++) {
+            ids[i] = i;
+        }
+        Node[] n = new Node[E];
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(bf.readLine());
+            n[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(n, (a, b) -> a.c - b.c);
+
+        int totalCost = 0;
+        int handledCount = 0;
+        for (int i = 0; i < E; i++) {
+            int s = n[i].s;
+            int e = n[i].e;
+            if (getUnion(s) != getUnion(e)) {
+                totalCost += n[i].c;
+                if (++handledCount == V - 1) {
+                    System.out.println(totalCost);
+                    return;
+                }
+                setUnion(s, e);
+            }
+        }
+        System.out.println("fiish");
+    }
+
+    private static int getUnion(int from) {
+        int f = from;
+        while (ids[f] != f) {
+            f = ids[f];
+        }
+
+        if (from != f) { // key !!! Union find 의 while loop를 줄임
+            ids[from] = f;
+        }
+
+        return f;
+    }
+
+    private static void setUnion(int from, int to) {
+        int f = from;
+        while (ids[f] != f) {
+            f = ids[f];
+        }
+        int t = to;
+        while (ids[t] != t) {
+            t = ids[t];
+        }
+        if (f > t) {
+            ids[f] = t;
+        } else {
+            ids[t] = f;
+        }
+    }
+
+    static class Node {
+        int s;
+        int e;
+        int c;
+
+        public Node(int s, int e, int c) {
+            this.s = s;
+            this.e = e;
+            this.c = c;
+        }
+    }
+}
