4ba06f2247ba9125c514eedd9b9375ba0759124b
iaaboy <dk6781.lee@gmail.com>
2024-12-01 15:12:41 +0900

    .

--- acmicpc10989/Main.java
+++ acmicpc10989/Main.java
@@ -2,6 +2,10 @@ package acmicpc10989;
 
 import java.io.*;
 
+/*
+ * https://www.acmicpc.net/problem/10989
+ */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +19,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 10001; i++) {
             for (int j = 0; j < arr[i]; j++) {
-                sb.append(String.format("%d\n", i));
+                sb.append(i).append("\n");
             }
         }
         System.out.print(sb);
