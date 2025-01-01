1726080690d3f39cf5563e17ce76d0122bc7d3cb
iaaboy <dk6781.lee@gmail.com>
2025-01-01 16:33:00 +0900

    진우의 비밀번호 풀이

--- acmicpc17205/Main.java
+++ acmicpc17205/Main.java
@@ -1,48 +1,28 @@
 package acmicpc17205;
 
 import java.io.*;
-import java.util.*;
 
 /* 진우의 비밀번호
  * https://www.acmicpc.net/problem/17205
  */
 
 public class Main {
-    static ArrayList<String> numList = new ArrayList<>();
-    static int N;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(bf.readLine());
-        String num = bf.readLine();
+        int N = Integer.parseInt(bf.readLine()) - 1;
+        char[] num = bf.readLine().toCharArray();
+        long ans = 0;
 
-        setNum(0, "");
-        numList.sort(null);
-        int index = getNum(num);
-        System.out.println(index);
-    }
-
-    private static int getNum(String num) {
-        int idx = 1;
-        for (String n : numList) {
-            if (num.contentEquals(n)) {
-                break;
+        for (int i = 0; i < num.length; i++) {
+            long diff = num[i] - 'a';
+            if (diff > 0) {
+                ans += diff * 26 * (Math.pow(26, N) - 1) / 25 + diff;
             }
-            idx++;
+            ans ++;
+            N--;
         }
-        return idx;
-    }
 
-    private static void setNum(int n, String cur) {
-        if (n == N) {
-            return;
-        }
-        for (int i = 0; i < 26; i++) {
-            char add = 'a';
-            add += i;
-            String nextStr = cur + add;
-            numList.add(nextStr);
-            setNum(n + 1, nextStr);
-        }
+        System.out.println(ans);
     }
 }
