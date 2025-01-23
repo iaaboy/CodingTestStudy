9275708b4f6879041246c31ec9e83a8d791270c8
iaaboy <dk6781.lee@gmail.com>
2025-01-23 20:55:58 +0900

    부분합 업데이트

--- acmicpc1806/Main.java
+++ acmicpc1806/Main.java
@@ -12,22 +12,20 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
-        long K = Long.parseLong(st.nextToken());
-        long[] arr = new long[N + 1];
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
         st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         int r = 0;
         int l = 0;
         long sum = arr[0];
         int count = Integer.MAX_VALUE;
+        if (sum >= K) {
+            count = 1;
+        }
         while (l < N && r < N) {
-            if (sum >= K) {
-                // System.out.println("*** ll " + l + ".." + r + ":" + sum + " .. " + (r - l +
-                // 1));
-                count = Math.min(count, r - l + 1);
-            }
             // System.out.println(l + ".." + r + ":" + sum);
             if (sum > K) {
                 sum -= arr[l];
