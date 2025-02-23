4e9adc59343adebd7989e27c42b93555f1eaebf4
iaaboy <dk6781.lee@gmail.com>
2025-02-23 15:51:55 +0900

    가장 긴 증가하는 부분 수열 4 풀이

--- acmicpc14002/Main.java
+++ acmicpc14002/Main.java
@@ -3,8 +3,8 @@ package acmicpc14002;
 import java.io.*;
 import java.util.*;
 
-/*
- * 실패
+/* 가장 긴 증가하는 부분 수열 4
+ * https://www.acmicpc.net/problem/14002
  */
 
 public class Main {
@@ -13,35 +13,33 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int num = Integer.parseInt(st.nextToken());
-        arr[0] = num;
-        int lis = 0;
-        for (int i = 1; i < N; i++) {
-            num = Integer.parseInt(st.nextToken());
-            int low = 0;
-            int high = lis;
-            if (num > arr[lis]) {
-                arr[++lis] = num;
-            } else {
-                while (low < high) {
-                    int mid = (low + high) / 2;
-                    if (arr[mid] < num) {
-                        low = mid + 1;
-                    } else {
-                        high = mid;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[N];
+        int max = 1;
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    if (max < dp[i]) {
+                        max = dp[i];
                     }
                 }
-                arr[low] = num;
             }
-            // System.out.println(lis + ": " + Arrays.toString(arr));
         }
-        System.out.println(lis + 1);
+        // System.out.println(Arrays.toString(dp));
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < lis + 1; i++) {
-            sb.append(arr[i] + " ");
+        int curMax = max;
+        for (int i = N - 1; i >= 0; i--) {
+            if (dp[i] == curMax) {
+                sb.insert(0, arr[i] + " ");
+                curMax--;
+            }
         }
-        String result = sb.substring(0, sb.length() - 1);
-        System.out.println(result);
-        br.close();
+        sb.insert(0, max + "\n").append("\n");
+        System.out.print(sb);
     }
 }
