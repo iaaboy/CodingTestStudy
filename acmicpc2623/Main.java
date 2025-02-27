23d190a2337e0d6f7440ea2667a20f64f7575050
iaaboy <dk6781.lee@gmail.com>
2025-02-27 23:47:01 +0900

    음악프로그램 풀이

--- acmicpc2623/Main.java
+++ acmicpc2623/Main.java
@@ -3,6 +3,15 @@ package acmicpc2623;
 import java.io.*;
 import java.util.*;
 
+/* 음악프로그램
+ * https://www.acmicpc.net/problem/2623
+ */
+
+ /*
+위상 정렬 알고리즘 기본.
+순환이 발생한 경우 0 을 출력하도록
+  */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -30,10 +39,10 @@ public class Main {
             }
         }
 
-        for (ArrayList<Integer> g : graph) {
-            System.out.println(g);
-        }
-        System.out.println(Arrays.toString(linkCount));
+        // for (ArrayList<Integer> g : graph) {
+        //     System.out.println(g);
+        // }
+        // System.out.println(Arrays.toString(linkCount));
 
         Queue<Integer> q = new ArrayDeque<>();
 
@@ -45,14 +54,15 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            // if (q.isEmpty()) {
-            //     continue;
-            // }
-            int v = q.remove();
+            if (q.isEmpty()) { //순환이 발생한 경우.
+                System.out.println(0);
+                return;
+            }
+            int v = q.poll();
             sb.append(v).append("\n");
             for (int nextV : graph.get(v)) {
                 linkCount[nextV]--;
-                if (linkCount[nextV] <= 0) {
+                if (linkCount[nextV] == 0) {
                     q.add(nextV);
                 }
             }
@@ -60,3 +70,10 @@ public class Main {
         System.out.print(sb);
     }
 }
+
+/*
+6 3
+3 1 4 3
+4 6 2 5 4
+3 2 3 4
+ */
