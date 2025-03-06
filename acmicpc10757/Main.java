e422c46cac021331cb6ed0712c1c06d7f9db400f
iaaboy <dk6781.lee@gmail.com>
2025-03-06 22:03:38 +0900

    피보나치 수 4 풀이

---
+++ acmicpc10757/Main.java
@@ -0,0 +1,34 @@
+package acmicpc10757;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        char[] a = st.nextToken().toCharArray();
+        char[] b = st.nextToken().toCharArray();
+
+        char[] result = charPlus(a, b);
+        System.out.println(result[0] == '0' ? new String(result, 1, result.length - 1) : new String(result));
+    }
+
+    private static char[] charPlus(char[] a, char[] b) {
+        int size = Math.max(a.length, b.length);
+        char[] result = new char[Math.max(a.length, b.length) + 1];
+        int carry = 0;
+        for (int i = 0; i < size + 1; i++) {
+            int sum = carry;
+            if (i < a.length) {
+                sum += a[a.length - 1 - i] - '0';
+            }
+            if (i < b.length) {
+                sum += b[b.length - 1 - i] - '0';
+            }
+            carry = sum / 10;
+            result[result.length - 1 - i] = (char) (sum % 10 + '0');
+        }
+        return result;
+    }
+}
--- acmicpc10826/Main.java
+++ acmicpc10826/Main.java
@@ -2,35 +2,49 @@ package acmicpc10826;
 
 import java.io.*;
 
+/* 피보나치 수 4
+ * https://www.acmicpc.net/problem/10826
+ */
+
 public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
 
-        String [] pivo = new String[n + 1];
-        pivo[0] = "0";
-        pivo[1] = "1";
-        for (int i = 2; i <= n; i++) {
-            pivo[i] = pivo[i - 2] + pivo[i - 1];
+        char[][] pivo = new char[n + 2][];
+        pivo[0] = new char[] { '0' };
+        pivo[1] = new char[] { '1' };
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) {
+                pivo[i] = charPlus(pivo[i - 1], pivo[i - 2]);
+            }
         }
-        System.out.println(pivo[n]);
+
+        String result = new String(pivo[n]);
+        while (result.length() > 1 && result.charAt(0) == '0') {
+            result = result.substring(1);
+        }
+        System.out.println(result);
     }
-    static String plus (String a, String b) {
-        int aSize = a.length();
-        int bSize = b.length();
-        StringBuilder sb = new StringBuilder();
 
-        int add = 0;
-        int length = Math.max(a.length(), b.length());
-        for (int i = 0; i < length ; i++) {
-            int aNum = a.charAt(a.length() - i) - '0';
-            int bNum = b.charAt(b.length() - i) - '0';
-            add += (aNum + bNum);
-            sb.append(add % 10);
-            add /= 10;
+    private static char[] charPlus(char[] a, char[] b) {
+        int size = Math.max(a.length, b.length);
+        char[] result = new char[Math.max(a.length, b.length) + 1];
+        int carry = 0;
+        for (int i = 0; i < size + 1; i++) {
+            int sum = carry;
+            if (i < a.length) {
+                sum += a[a.length - 1 - i] - '0';
+            }
+            if (i < b.length) {
+                sum += b[b.length - 1 - i] - '0';
+            }
+            carry = sum / 10;
+            result[result.length - 1 - i] = (char) (sum % 10 + '0');
         }
-
-
-        return "";
+        if (result[0] == '0') {
+            return new String(result, 1, result.length - 1).toCharArray();
+        }
+        return result;
     }
 }
