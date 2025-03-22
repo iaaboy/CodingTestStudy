c8b94c912c7ea62dc266953885a32a6107ca5028
iaaboy <dk6781.lee@gmail.com>
2025-03-22 14:09:03 +0900

    구름다리 2 풀이

--- acmicpc23255/Main.java
+++ acmicpc23255/Main.java
@@ -3,6 +3,14 @@ package acmicpc23255;
 import java.io.*;
 import java.util.*;
 
+/* 구름다리 2
+ * https://www.acmicpc.net/problem/23255
+ 
+ id가 작은것부터, 
+ 큰 순서대로 실행.
+ 주위에 있는 id중 가능한 가장 작은 값을 내 id로 지정.
+ */
+
 public class Main {
     static int[] arr;
 
@@ -12,37 +20,41 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         arr = new int[N + 1];
-        int []idIndex = new int[N + 1];
+        List<Integer>[] graph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            arr[i] = i;
+            graph[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(bf.readLine());
-            int parent = Integer.parseInt(st.nextToken());
-            int child = Integer.parseInt(st.nextToken());
-            arr[child] = parent;
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from].add(to);
+            graph[to].add(from);
         }
-        for (int i = 1; i <= N; i++) {
-            setUnion(i);
-        }
-        System.out.println(Arrays.toString(arr));
+
+        int[] color = new int[N + 1];
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            arr[i] = ++idIndex[arr[i]];
-            sb.append(arr[i]).append(" ");
-        }
-        System.out.println(sb);
-    }
+            TreeSet<Integer> near = new TreeSet<>();
+            for (int j : graph[i]) {
+                if (color[j] != 0) {
+                    near.add(color[j]);
+                }
+            }
 
-    private static void setUnion(int currentId) {
-        int root = arr[currentId];
-        while (arr[root] != root) {
-            root = arr[root];
+            // System.out.println(i + ":" + near);
+            int c = 1;
+            for (int j : near) {
+                if (j != c) {
+                    break;
+                }
+                c++;
+            }
+            color[i] = c;
+            sb.append(color[i]).append(" ");
         }
-        while (arr[currentId] != root) {
-            int prev = currentId;
-            currentId = arr[currentId];
-            arr[prev] = root;
-        }
+
+        System.out.println(sb);
     }
 }
