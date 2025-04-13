14291a013ae7fa9778b79f4c0fafb69014743aa9
iaaboy <dk6781.lee@gmail.com>
2025-04-13 22:06:22 +0900

    bronz 풀이

--- baseForm/Main.java
+++ baseForm/Main.java
@@ -1,26 +1,21 @@
 package baseForm;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-
-        StringBuilder sb = new StringBuilder();
-        while (!(a == 0 && b == 0 && c == 0 && d == 0)) {
-            sb.append(c-b).append(" ").append(d-a).append("\n");
-            st = new StringTokenizer(bf.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            d = Integer.parseInt(st.nextToken());
+        String a = bf.readLine();
+        String b = bf.readLine();
+        if (a.contentEquals("null")) {
+            System.out.println("NullPointerException\nNullPointerException");
+        } else {
+            if (b.contentEquals("null")) {
+                b = null;
+            }
+            System.out.println(a.equals(b));
+            System.out.println(a.equalsIgnoreCase(b));
         }
-        System.out.print(sb);
     }
 }
