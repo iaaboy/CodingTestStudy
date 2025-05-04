6c4566594a58a062dcb1afbbc34e0d57e47935eb
iaaboy <dk6781.lee@gmail.com>
2025-05-04 09:24:25 +0900

    BaseInputTemplate

---
+++ 0_BaseInputTemplate/Main.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+    }
+}
