257dbc107435d0611feeef481fc65865393c4f5a
DongKyuLee <dk6781.lee@gmail.com>
2025-07-13 10:57:36 +0900

    LCA 2 풀이

--- acmicpc11438/Main.java
+++ acmicpc11438/Main.java
@@ -3,12 +3,14 @@ package acmicpc11438;
 import java.io.*;
 import java.util.*;
 
-/*
- * 풀이중
+/* LCA 2
+ * https://www.acmicpc.net/problem/11438
  */
 
 public class Main {
     static Vertex[] v;
+    static int[][] P;
+    static int dpSize;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -26,51 +28,89 @@ public class Main {
             v[son].node.add(parent);
         }
 
-        StringBuilder debugSb = new StringBuilder();
-
-        //0. parent를 set
+        // 0. parent를 set
         v[0].parent = 0;
         v[1].parent = 0;
         setParent(0, 1, 1);
 
-        //1 깊이 구한다.
-        //2 바로 위 부모노드 구한다.
-        //  depth를 구할때 부모노드 array를 업데이트
+        // 1 깊이 구한다.
+        // 2 바로 위 부모노드 구한다.
+        // depth를 구할때 부모노드 array를 업데이트
         int depth = getDepth(1) - 1;
-        debugSb.append("depth: " + depth + "\n");
-        int dpSize = getDpSize(depth);
-        debugSb.append("dpSize: " + dpSize + "\n");
+        dpSize = getDpSize(depth);
 
-        //3 부모노드 dp점화식 구하기.
+        // 3 부모노드 dp점화식 구하기.
         // P[K][N] = P[K-1][ P[K-1][N] ]
 
-        //3-1 dp 초기값
-        int [][]P = new int[dpSize][N + 1];
-        for (int i = 0; i <= N; i++) {
-            P[0][i] = v[i].parent;
-            for (int k = 1; k < dpSize; k++) {
-                P[k][i] = P[k-1][ P[k-1][i]];
-            }
-        }
-        for (int i = 0; i < dpSize; i++) {
-            for (int j = 0; j <= N; j++) {
-                debugSb.append(P[i][j] + " ");
-            }
-            debugSb.append("\n");
-        }
-        
+        // 3-1 dp 초기값
 
+        P = new int[dpSize][N + 1];
+        for (int i = 1; i <= N; i++) {
+            P[0][i] = v[i].parent;
+        }
+        for (int k = 1; k < dpSize; k++) {
+            for (int i = 1; i <= N; i++) {
+                P[k][i] = P[k - 1][P[k - 1][i]];
+            }
+        }
+
+        // StringBuilder debugSb = new StringBuilder();
+        // debugSb.append(Arrays.toString(v)).append("\n");
+        // debugSb.append("depth: " + depth + "\n");
+        // debugSb.append("dpSize: " + dpSize + "\n");
+        // for (int i = 0; i < dpSize; i++) {
+        //     for (int j = 0; j <= N; j++) {
+        //         debugSb.append(P[i][j] + " ");
+        //     }
+        //     debugSb.append("\n");
+        // }
+        // System.out.print(debugSb);
 
         int M = Integer.parseInt(bf.readLine());
         for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int me = Integer.parseInt(st.nextToken());
             int you = Integer.parseInt(st.nextToken());
-
-
+            // me와 you의 공통조상 찾아서 프린트
+            int lca = getLca(me, you);
+            sb.append(lca).append("\n");
         }
         System.out.print(sb);
-        System.out.print(debugSb);
+
+    }
+
+    private static int getLca(int me, int you) {
+        int a = 0;
+        int b = 0;
+        if (v[me].depth > v[you].depth) {
+            a = you;
+            b = me;
+        } else {
+            a = me;
+            b = you;
+        }
+
+        // 깊이 맞추기
+        for (int k = dpSize - 1; k >= 0; k--) {
+            if (Math.pow(2, k) <= v[b].depth - v[a].depth) {
+                b = P[k][b];
+            }
+        }
+
+        // 동시에 올라가면서 조상 찾기
+        for (int k = dpSize - 1; k >= 0; k--) {
+            if (P[k][a] != P[k][b]) {
+                a = P[k][a];
+                b = P[k][b];
+            }
+        }
+
+        int lca = a;
+        if (a != b) {
+            lca = P[0][lca];
+        }
+
+        return lca;
     }
 
     private static void setParent(int parent, int current, int depth) {
@@ -86,7 +126,7 @@ public class Main {
     private static int getDpSize(int depth) {
         int dpSize = 0;
         while (depth > 0) {
-            depth  /= 2;
+            depth /= 2;
             dpSize++;
         }
         return dpSize;
@@ -94,9 +134,9 @@ public class Main {
 
     private static int getDepth(int node) {
         int myRemainedDepth = 0;
-      for (Vertex v : v) {
-        myRemainedDepth = Math.max(myRemainedDepth, v.depth);
-      }
+        for (Vertex v : v) {
+            myRemainedDepth = Math.max(myRemainedDepth, v.depth);
+        }
         return myRemainedDepth + 1;
     }
 
@@ -104,5 +144,9 @@ public class Main {
         int parent = -1;
         int depth = 0;
         ArrayList<Integer> node = new ArrayList<>();
+        @Override
+        public String toString() {
+            return parent + " , " + depth + " , " + node.toString() + "\n";
+        }
     }
 }
