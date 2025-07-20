eefd575429ebaf41c48d1971d0e3800d4ce646a7
iaaboy <dk6781.lee@gmail.com>
2025-07-20 14:41:16 +0900

    개미 풀이

--- acmicpc10158/Main.java
+++ acmicpc10158/Main.java
@@ -3,8 +3,9 @@ package acmicpc10158;
 import java.io.*;
 import java.util.*;
 
-/*
- * 풀이중
+/* 개미
+ * https://www.acmicpc.net/problem/10158
+ * 수학문제 .. for문을 더 간결할게 할 수 있다.
  */
 
 public class Main {
@@ -19,43 +20,29 @@ public class Main {
         int y = Integer.parseInt(st.nextToken());// 세로
 
         int t = Integer.parseInt(bf.readLine());
-        int[] dx = { 1, -1, -1, 1 };
-        int[] dy = { 1, 1, -1, -1 };
-        int direction = 0;
-        while (t-- > 0) {
-            if ((x == 0 || x == w) && (y == 0 || y == h)) {
-                // 이전 방향으로
-                direction += 2;
-                direction %= 4;
-            } else if (x == w) {
-                //이전 방향 0, 4
-                if (direction == 0) {
-                    direction = 1;
-                } else if (direction == 3) {
-                    direction = 2;
-                }
-            } else if (y == h) {
-                if (direction == 0) {
-                    direction = 3;
-                } else if (direction == 1) {
-                    direction = 2;
-                }
-            } else if (x == 0) {
-                if (direction == 2) {
-                    direction = 3;
-                } else if (direction == 1) {
-                    direction = 0;
-                }
-            } else if (y == 0) {
-                if (direction == 3) {
-                    direction = 0;
-                } else if (direction == 2) {
-                    direction = 1;
-                }
+        int tw = t % (w * 2);
+        int th = t % (h * 2);
+        boolean dirPlus = true;
+        for (int i = 0; i < tw; i++) {
+            if (x == w || x == 0) {
+                dirPlus = !dirPlus;
             }
-            x += dx[direction];
-            y += dy[direction];
-            // System.out.println(x + " " + y);
+            if (dirPlus) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        dirPlus = true;
+        for (int i = 0; i < th; i++) {
+            if (y == h || y == 0) {
+                dirPlus = !dirPlus;
+            }
+            if (dirPlus) {
+                y++;
+            } else {
+                y--;
+            }
         }
         System.out.println(x + " " + y);
     }
