20c06ffd045287fdc243937c26c146655531b34a
iaaboy <dk6781.lee@gmail.com>
2025-10-11 01:16:52 +0900

    정육점 풀이 애드호

---
+++ acmicpc3711/Main.kt
@@ -0,0 +1,43 @@
+package acmicpc3711
+
+import java.io.*
+import java.util.*
+
+/* 정육점
+https://www.acmicpc.net/problem/2258
+ */
+
+fun main(args: Array<String>) {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val st = StringTokenizer(bf.readLine())
+    val T = st.nextToken().toInt()
+    val maxNumber = 1000000
+
+//    println(primeNumbers)
+
+    val sb = StringBuilder()
+    repeat(T) {
+        val G = bf.readLine().toInt()
+        val arr = Array(G) { bf.readLine().toInt() }
+        val hashSet = HashSet<Int>()
+        for (p in 1..maxNumber) {
+            var isComplete = true
+            for (num in arr) {
+                val remain = num % p
+                if (hashSet.contains(remain)) {
+                    hashSet.clear()
+                    isComplete = false
+                    break
+                }
+                hashSet.add(num % p)
+            }
+            hashSet.clear()
+            if (isComplete) {
+                sb.append(p).append("\n")
+                break
+            }
+
+        }
+    }
+    print(sb)
+}
