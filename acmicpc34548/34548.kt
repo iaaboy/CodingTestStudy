395e1b01b863a3ef51431fcb00d4025df308f514
iaaboy <dk6781.lee@gmail.com>
2025-11-08 15:41:04 +0900

    와우 네트워크 풀이, union find

--- acmicpc34548/34548.kt
+++ acmicpc34548/34548.kt
@@ -3,6 +3,10 @@ package acmicpc34548
 import java.io.*
 import java.util.*
 
+/* 와우 네트워크
+https://www.acmicpc.net/problem/34548
+ */
+
 fun main() {
     val bf = BufferedReader(InputStreamReader(System.`in`))
     var st = StringTokenizer(bf.readLine())
@@ -16,27 +20,35 @@ fun main() {
     }
     edges.sort()
     ids = IntArray(N) { it }
-
     var groupCount = N
-    val groupCounts = IntArray(T + 1)
-    groupCounts[1] = groupCount
+    val groupCounts = ArrayList<IntArray>()
+    groupCounts.add(intArrayOf(1, N))
+    var prevHandled = 1
     for (edge in edges) {
         if (getUnion(edge.a) != getUnion(edge.b)) {
             setUnion(edge.a, edge.b)
             groupCount--
 //            println("${edge.time} : $groupCount")
-            groupCounts[edge.time] = groupCount
+            if (prevHandled == edge.time) {
+                groupCounts.last()[1] = groupCount
+            } else {
+                groupCounts.add(intArrayOf(edge.time, groupCount))
+            }
+            prevHandled = edge.time
         }
     }
-    var sum = 0
-    for (i in 1..T) {
-        if (groupCounts[i] == 0) {
-            groupCounts[i] = groupCounts[i - 1]
-        }
-        sum += groupCounts[i]
-    }
+    groupCounts.add(intArrayOf(T + 1, groupCount))
 
-//    println(groupCounts.contentToString())
+    var sum = 0L
+    for (i in 1 until groupCounts.size) {
+        val count = groupCounts[i - 1][1]
+        val time = groupCounts[i][0] - groupCounts[i - 1][0]
+        sum += count.toLong() * time.toLong()
+    }
+//    for (ints in groupCounts) {
+//        print("${ints[0]} ${ints[1]} - ")
+//    }
+
     println(sum)
 }
 
