4faa0aef928cff4e8f555186205239b0781bc679
iaaboy <dk6781.lee@gmail.com>
2026-01-19 06:55:56 +0900

    시간 관리하기, 정렬, 그리디

---
+++ acmicpc6068/6068.kt
@@ -0,0 +1,52 @@
+package acmicpc6068
+
+
+import java.io.*
+import java.util.*
+
+/* 시간 관리하기, 정렬, 그리디
+https://www.acmicpc.net/problem/6068
+ */
+
+fun main() {
+    val bf = BufferedReader(InputStreamReader(System.`in`))
+    val n = bf.readLine().toInt()
+    val pq = PriorityQueue<Work>{a,b ->
+        b.end - a.end
+    }
+    var lastPoint = 0
+    repeat(n) {
+        val st = StringTokenizer(bf.readLine())
+        val start = st.nextToken().toInt()
+        val end = st.nextToken().toInt()
+        pq.add(Work(end - start, end))
+        lastPoint = lastPoint.coerceAtLeast(end)
+    }
+
+    var me = pq.poll()
+    while (pq.isNotEmpty()) {
+        val you = pq.poll()
+        if(you.end > me.start) {
+            //합친다.
+
+            val youStart = you.start - (me.end - me.start)
+            val myStart = me.start - (you.end - you.start)
+
+            if (youStart > myStart) {
+                me.start = youStart
+                me.end = you.end
+            } else {
+                me.start = myStart
+                me.end = me.end
+            }
+        } else {
+            me = you
+        }
+//        println(me)
+    }
+
+    if (me.start < 0) me.start = -1
+
+    println(me.start)
+}
+private data class Work(var start : Int, var end : Int)
